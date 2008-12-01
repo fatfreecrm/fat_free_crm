444e10ca9d60a56f9dd19489f890c91b8da095bd
Mike Dvorkin <mike@dvorkin.net>
2008-11-30 21:54:12 -0800

    More work on [accounts] model, CSS styles, and [add account] form

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,7 @@
 class Account < ActiveRecord::Base
+  acts_as_paranoid
+  validates_presence_of :name
+  validates_uniqueness_of :name, :scope => :user
+  
+  attr_accessor :tags # stub
 end
--- db/migrate/004_create_accounts.rb
+++ db/migrate/004_create_accounts.rb
@@ -1,9 +1,23 @@
 class CreateAccounts < ActiveRecord::Migration
   def self.up
-    create_table :accounts do |t|
-
+    create_table :accounts, :force => true do |t|
+      t.string      :uuid, :limit => 36
+      t.references  :user
+      t.string      :name, :limit => 64, :null => false, :default => ""
+      t.string      :access, :limit => 8, :default => "Private" # %w(Private Public Shared)
+      t.string      :notes
+      t.string      :website, :limit => 64
+      t.string      :phone, :limit => 32
+      t.string      :fax, :limit => 32
+      t.string      :billing_address
+      t.string      :shipping_address
+      t.datetime    :deleted_at
       t.timestamps
     end
+
+    add_index :accounts, [ :user_id, :name ], :unique => true
+    add_index :accounts, :uuid
+    ActiveRecord::Base.connection.execute("CREATE TRIGGER accounts_uuid BEFORE INSERT ON accounts FOR EACH ROW SET NEW.uuid = UUID()");
   end
 
   def self.down
