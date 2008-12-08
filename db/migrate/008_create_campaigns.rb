bcc10d1be9eae37c286692ea6e17c0e110b6671c
Mike Dvorkin <mike@dvorkin.net>
2008-12-07 21:51:58 -0800

    Started with [Campaigns/New], integrated calendar_date_select for start/end dates

--- db/migrate/008_create_campaigns.rb
+++ db/migrate/008_create_campaigns.rb
@@ -1,9 +1,25 @@
 class CreateCampaigns < ActiveRecord::Migration
   def self.up
-    create_table :campaigns do |t|
-
+    create_table :campaigns, :force => true do |t|
+      t.string      :uuid,   :limit => 36
+      t.references  :user
+      t.string      :name,   :limit => 64, :null => false, :default => ""
+      t.string      :type,   :limit => 64
+      t.string      :stage,  :limit => 64
+      t.decimal     :budget,           :precision => 12, :scale => 2
+      t.decimal     :actual_cost,      :precision => 12, :scale => 2
+      t.decimal     :expected_revenue, :precision => 12, :scale => 2
+      t.integer     :expected_leads
+      t.date        :starts_on
+      t.date        :ends_on
+      t.text        :objective
+      t.datetime    :deleted_at
       t.timestamps
     end
+
+    add_index :campaigns, [ :user_id, :name, :deleted_at ], :unique => true
+    add_index :campaigns, :uuid
+    ActiveRecord::Base.connection.execute("CREATE TRIGGER campaigns_uuid BEFORE INSERT ON campaigns FOR EACH ROW SET NEW.uuid = UUID()");
   end
 
   def self.down
