440b202a52468f43e6d7446d7f0f91d8bc72739f
Mike Dvorkin <mike@dvorkin.net>
2008-12-09 22:21:37 -0800

    Implemented [Campaing/New] with dates validation and status set

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,7 @@ class Account < ActiveRecord::Base
   belongs_to :user
   has_many :permissions, :as => :asset, :include => :user
   acts_as_paranoid
+
   validates_presence_of :name, :message => "^Please specify account name."
   validates_uniqueness_of :name, :scope => :user_id
 
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,23 @@
 class Campaign < ActiveRecord::Base
   belongs_to :user
+  acts_as_paranoid
+
+  validates_presence_of :name, :message => "^Please specify campaign name."
+  validates_uniqueness_of :name, :scope => :user_id
+  before_create :set_campaign_status
+
+  # Make sure end date > start date.
+  #----------------------------------------------------------------------------
+  def validate
+    if (self[:starts_on] && self[:ends_on]) && (self[:starts_on] > self[:ends_on])
+      errors.add(:ends_on, "^Please make sure the campaign end date is after the start date.")
+    end
+  end
+
+  private
+  #----------------------------------------------------------------------------
+  def set_campaign_status
+    self[:status] = self[:starts_on] && (self[:starts_on] <= Date.today) ? "Started" : "Planned"
+  end
+
 end
