6b66e022a40ca7c8d7846b24d01ab3263c4315d2
Mike Dvorkin <mike@dvorkin.net>
2008-12-10 23:17:21 -0800

    More work on list of campaigns page

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -6,7 +6,7 @@ class CampaignsController < ApplicationController
   # GET /campaigns.xml
   #----------------------------------------------------------------------------
   def index
-    @campaigns = Campaign.find(:all)
+    @campaigns = Campaign.find(:all, :order => "id DESC")
 
     respond_to do |format|
       format.html # index.html.erb
