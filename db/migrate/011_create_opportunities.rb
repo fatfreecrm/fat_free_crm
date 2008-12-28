4dad82f86e06e92a4e175d10c4b87c375cacb366
Mike Dvorkin <mike@dvorkin.net>
2008-12-27 19:25:56 -0800

    Added [contacts] and [opportunities] database migrations

---
+++ db/migrate/011_create_opportunities.rb
@@ -0,0 +1,28 @@
+class CreateOpportunities < ActiveRecord::Migration
+  def self.up
+    create_table :opportunities, :force => true do |t|
+      t.string      :uuid,    :limit => 36
+      t.references  :user
+      t.references  :account, :null => false
+      t.references  :campaign
+      t.string      :name,    :limit => 64, :null => false, :default => ""
+      t.string      :source,  :limit => 32
+      t.string      :stage,   :limit => 32
+      t.integer     :probability
+      t.decimal     :amount,           :precision => 12, :scale => 2
+      t.decimal     :expected_revenue, :precision => 12, :scale => 2
+      t.date        :close_on
+      t.datetime    :deleted_at
+      t.text        :notes
+      t.timestamps
+    end
+
+    add_index :opportunities, [ :user_id, :deleted_at ], :unique => true
+    add_index :opportunities, :uuid
+    ActiveRecord::Base.connection.execute("CREATE TRIGGER opportunities_uuid BEFORE INSERT ON opportunities FOR EACH ROW SET NEW.uuid = UUID()");
+  end
+
+  def self.down
+    drop_table :opportunities
+  end
+end
