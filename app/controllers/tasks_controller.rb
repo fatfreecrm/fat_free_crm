1580c0823bf50a785b58a8f4c8d0e8b7d7b02c9f
Mike Dvorkin <mike@dvorkin.net>
2009-02-07 18:37:42 -0800

    Implemented task#create except picking specific date from the calendar

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
+  AJAX_REQUESTS = [ :new, :create, :destroy, :complete, :filter ]
   before_filter :require_user
-  before_filter :get_data_for_sidebar, :only => [ :index, :destroy, :complete ]
-  before_filter "set_current_tab(:tasks)"
+  before_filter :update_sidebar, :only => :index
+  before_filter "set_current_tab(:tasks)", :except => AJAX_REQUESTS
 
   # GET /tasks
   # GET /tasks.xml
@@ -61,7 +62,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        get_data_for_sidebar
+        update_sidebar
         format.js   # create.js.rjs
         format.html { redirect_to(@task) }
         format.xml  { render :xml => @task, :status => :created, :location => @task }
@@ -101,6 +102,7 @@ class TasksController < ApplicationController
     # Make sure bucket's div gets hidden if we're deleting last task in the bucket.
     @bucket = Task.bucket(@current_user, params[:bucket],  params[:view])
 
+    update_sidebar
     respond_to do |format|
       format.js   # destroy.js.rjs
       format.html { redirect_to(tasks_url) }
@@ -118,6 +120,7 @@ class TasksController < ApplicationController
     # Make sure bucket's div gets hidden if it's the last completed task in the bucket.
     @bucket = Task.bucket(@current_user, params[:bucket])
 
+    update_sidebar
     respond_to do |format|
       format.js   # complete.js.rjs
       format.html { redirect_to(@task) }
@@ -128,26 +131,45 @@ class TasksController < ApplicationController
   # Ajax request to filter out list of tasks.
   #----------------------------------------------------------------------------
   def filter
-    name = "filter_by_task_#{params[:view]}"
-    filters = (session[name].nil? ? [] : session[name].split(","))
-    if params[:checked] == "true"
-      filters << params[:filter]
-    else
-      filters.delete(params[:filter])
+    update_session do |filters|
+      if params[:checked] == "true"
+        filters << params[:filter]
+      else
+        filters.delete(params[:filter])
+      end
     end
-    session[name] = filters.join(",")
   end
 
   private
 
-  # Dispatch to appropriate sidebar handler, then save filters with non-zero
-  # task counts in a session.
+  # Yields array of current filters and updates the session using new values.
   #----------------------------------------------------------------------------
-  def get_data_for_sidebar
+  def update_session
+    name = "filter_by_task_#{@view}"
+    filters = (session[name].nil? ? [] : session[name].split(","))
+    yield filters
+    session[name] = filters.join(",")
+  end
+
+  # Collect data necessary to render filters sidebar.
+  #----------------------------------------------------------------------------
+  def update_sidebar
     @view = params[:view]
     @view = "pending" unless %w(pending assigned completed).include?(@view)
     @task_total = Task.totals(@current_user, @view)
 
+    # Update filters session if we added, deleted, or completed a task.
+    if @task
+      update_session do |filters|
+        if !@task.deleted_at && !@task.completed_at # created new task
+          filters << @task.due_date
+        elsif @bucket # deleted or completed and need to hide a bucket
+          filters.delete(params[:bucket])
+        end
+      end
+    end
+
+    # Create default filters if filters session is empty.
     name = "filter_by_task_#{@view}"
     unless session[name]
       filters = @task_total.keys.select { |key| key != :all && @task_total[key] != 0 }.join(",")
