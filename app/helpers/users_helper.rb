d7d36c068c484969eba2a43d02501439b76eca8e
Mike Dvorkin <mike@dvorkin.net>
2009-02-09 21:48:19 -0800

    Converted remaining forms to HAML and the stylesheet to SASS

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+  def toggle_open_id_signup
+    <<-EOF
+    $("login").toggle();
+    $("openid").toggle();
+    $("login_link").toggle();
+    $("openid_link").toggle();
+    $('user_email').focus()
+    EOF
+  end
+
 end
