2934a22a73a7724c038e5ad4ea6c48a16e793008
Mike Dvorkin <mike@dvorkin.net>
2009-02-13 21:48:37 -0800

    Implemented Campaign Summary sidebar for the campaign landing page

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -7,4 +7,18 @@ module CampaignsHelper
     check_box_tag("status[]", status, checked, :onclick => remote_function(:url => { :action => :filter }, :with => %Q/"status=" + $$("input[name='status[]']").findAll(function (el) { return el.checked }).pluck("value")/))
   end
 
+  #----------------------------------------------------------------------------
+  def performance(actual, target)
+    if target.to_i > 0 && actual.to_i > 0
+      if target > actual
+        n = 100 - actual * 100 / target
+        html = content_tag(:span, "(-#{number_to_percentage(n, :precision => 1)})", :class => "warn")
+      else
+        n = actual * 100 / target - 100
+        html = content_tag(:span, "(+#{number_to_percentage(n, :precision => 1)})", :class => "cool")
+      end
+    end
+    html || ""
+  end
+
 end
