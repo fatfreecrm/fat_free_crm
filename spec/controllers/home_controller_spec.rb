b6193c4ff4119fd70d5c2f4bc9c43b3e8e709ba1
Mike Dvorkin <mike@dvorkin.net>
2009-03-14 16:41:19 -0700

    Added home controller spec [#15 state:open]

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,9 +2,26 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe HomeController do
 
-  #Delete this example and add some real ones
-  it "should use HomeController" do
-    controller.should be_an_instance_of(HomeController)
+  it "should assign @hello and call hook" do
+    require_user
+    controller.should_receive(:hook).at_least(:once)
+
+    get :index
+    assigns[:hello] = "world"
+  end
+
+  it "should toggle expand/collapse state of form section in the session (delete existing session key)" do
+    session[:hello] = "world"
+
+    xhr :get, :toggle, :id => "hello"
+    session.data.keys.should_not include(:hello)
+  end
+
+  it "should toggle expand/collapse state of form section in the session (save new session key)" do
+    session.data.delete(:hello)
+
+    xhr :get, :toggle, :id => "hello"
+    session[:hello] = "world"
   end
 
 end
