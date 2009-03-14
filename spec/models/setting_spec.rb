96faa3dccc73d454da7395d59da41ce38e1c47b6
Mike Dvorkin <mike@dvorkin.net>
2009-03-14 11:01:09 -0700

    Added spec for Settings model [#15 state:open]

--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -14,12 +14,30 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Setting do
-  before(:each) do
-    @valid_attributes = {
-    }
-  end
 
   it "should create a new instance given valid attributes" do
-    Setting.create!(@valid_attributes)
+    Setting.create!(:name => "name", :value => "value", :default_value => nil)
+  end
+
+  it "should find existing setting by its name using [] or method notations" do
+    @setting = Factory(:setting, :name => "thingymabob", :value => Base64.encode64(Marshal.dump("magoody")))
+    Setting[:thingymabob].should == "magoody"
+    Setting.thingymabob.should == "magoody"
+  end
+
+  it "should use :default_value if the value is missing" do
+    @setting = Factory(:setting, :name => "magoody", :value => nil, :default_value => Base64.encode64(Marshal.dump("thingymabob")))
+    Setting[:magoody].should == "thingymabob"
+    Setting.magoody.should == "thingymabob"
+  end
+
+  it "should save a new value of a setting using []= or method notation" do
+    Setting[:hello] = "world"
+    Setting[:hello].should == "world"
+    Setting.hello.should == "world"
+
+    Setting.world = "hello"
+    Setting.world.should == "hello"
+    Setting[:world].should == "hello"
   end
 end
