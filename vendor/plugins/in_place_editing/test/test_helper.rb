91cc1d0d223316448ca0b287e41c95df9d526c8d
Mike Dvorkin <mike@dvorkin.net>
2009-03-24 20:37:30 -0700

    Removed unused plugins [#24 state:open]

--- vendor/plugins/in_place_editing/test/test_helper.rb
+++
@@ -1,8 +0,0 @@
-$:.unshift(File.dirname(__FILE__) + '/../lib')
-
-require 'test/unit'
-require 'rubygems'
-require 'action_controller'
-require 'action_controller/assertions'
-require 'in_place_editing'
-require 'in_place_macros_helper'
