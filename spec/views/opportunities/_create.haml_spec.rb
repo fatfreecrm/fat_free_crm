89c44c100aa24bd8cb104e75fd0ea138bb52fbf0
Mike Dvorkin <mike@dvorkin.net>
2009-05-29 21:07:43 -0700

    Fixed opportunity assignment issue reported by Deepu (LH #49) [#49 state:open]

--- spec/views/opportunities/_create.haml_spec.rb
+++ spec/views/opportunities/_create.haml_spec.rb
@@ -5,15 +5,26 @@ describe "/opportunities/_create.html.haml" do
 
   before(:each) do
     login_and_assign
+    assigns[:opportunity] = Factory.build(:opportunity)
+    @account = Factory(:account)
+    assigns[:account] = @account
+    assigns[:accounts] = [ @account ]
+    assigns[:users] = [ @current_user ]
   end
 
   it "should render [create opportunity] form" do
-    assigns[:opportunity] = Factory.build(:opportunity)
     template.should_receive(:render).with(hash_including(:partial => "opportunities/top_section"))
     template.should_receive(:render).with(hash_including(:partial => "opportunities/permissions"))
 
     render "/opportunities/_create.html.haml"
     response.should have_tag("form[class=new_opportunity]")
+  end
+
+  it "should pick default assignee (Myself)" do
+    render "/opportunities/_create.html.haml"
+    response.should have_tag("select[id=opportunity_assigned_to]") do |options|
+      options.to_s.should_not include_text(%Q/selected="selected"/)
+    end
   end
 end
 
--- spec/views/opportunities/_edit.haml_spec.rb
+++ spec/views/opportunities/_edit.haml_spec.rb
@@ -6,17 +6,36 @@ describe "/opportunities/edit.html.erb" do
   before(:each) do
     login_and_assign
     @account = Factory(:account)
-    assigns[:opportunity] = Factory(:opportunity)
-    assigns[:users] = [ @current_user ]
     assigns[:account] = @account
     assigns[:accounts] = [ @account ]
   end
 
   it "should render [edit opportunity] form" do
-    @form = mock("form")
+    assigns[:users] = [ @current_user ]
+    assigns[:opportunity] = Factory(:opportunity)
+    render "/opportunities/_edit.html.haml"
+    response.should have_tag("form[class=edit_opportunity]")
+  end
+
+  it "should pick default assignee (Myself)" do
+    assigns[:users] = [ @current_user ]
+    assigns[:opportunity] = Factory(:opportunity, :assignee => nil)
     render "/opportunities/_edit.html.haml"
 
-    response.should have_tag("form[class=edit_opportunity]")
+    response.should have_tag("select[id=opportunity_assigned_to]") do |options|
+      options.to_s.should_not include_text(%Q/selected="selected"/)
+    end
+  end
+
+  it "should show correct assignee" do
+    @user = Factory(:user)
+    assigns[:users] = [ @current_user, @user ]
+    assigns[:opportunity] = Factory(:opportunity, :assignee => @user)
+    render "/opportunities/_edit.html.haml"
+
+    response.should have_tag("select[id=opportunity_assigned_to]") do |options|
+      options.to_s.should include_text(%Q/<option selected="selected" value="#{@user.id}">/)
+    end
   end
 
 end
