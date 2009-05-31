9a51d20af8aff934478a46d70fb50c9e6b9d5450
Mike Dvorkin <mike@dvorkin.net>
2009-05-30 20:07:25 -0700

    Implemented crm.Menu class and added menu related CSS styles [#46 state:open]

--- public/javascripts/crm_classes.js
+++ public/javascripts/crm_classes.js
@@ -6,91 +6,187 @@ crm.Popup = Class.create({
 
   //----------------------------------------------------------------------------
   initialize: function() {
-		this.options = Object.extend({
-			trigger     : "trigger",                // #id of the element that triggers on_mouseover popup.
+    this.options = Object.extend({
+      trigger     : "trigger",                // #id of the element that triggers on_mouseover popup.
       target      : "popup",                  // #id of the popup div that is shown or hidden.
-			appear      : 0,                        // duration of EffectAppear or 0 for show().
-			fade        : 0,                        // duration of EffectFade or 0 for hide().
-			under       : false,                    // true to show popup right under the trigger div.
-			zindex      : 100,                      // zIndex value for the popup.
-			before_show : Prototype.emptyFunction,  // before show callback.
-			before_hide : Prototype.emptyFunction,  // before hide callback.
-			after_show  : Prototype.emptyFunction,  // after show callback.
-			after_hide  : Prototype.emptyFunction   // after hide callback.
-		}, arguments[0] || { });
+      appear      : 0,                        // duration of EffectAppear or 0 for show().
+      fade        : 0,                        // duration of EffectFade or 0 for hide().
+      under       : false,                    // true to show popup right under the trigger div.
+      zindex      : 100,                      // zIndex value for the popup.
+      before_show : Prototype.emptyFunction,  // before show callback.
+      before_hide : Prototype.emptyFunction,  // before hide callback.
+      after_show  : Prototype.emptyFunction,  // after show callback.
+      after_hide  : Prototype.emptyFunction   // after hide callback.
+    }, arguments[0] || {});
 
-		this.popup = $(this.options.target);      // actual popup div.
+    this.popup = $(this.options.target);      // actual popup div.
 
     this.setup_show_observer();
     this.setup_toggle_observer();
     this.setup_hide_observer();
-	},
-
-  //----------------------------------------------------------------------------
-	setup_show_observer: function() {
-		$(this.options.trigger).observe("mouseover", function(e) {
-			if (this.popup && !this.popup.visible()) {
-			  this.show_popup(e);
-		  }
-		}.bind(this));
   },
 
   //----------------------------------------------------------------------------
-	setup_toggle_observer: function() {
-		$(this.options.trigger).observe("click", function(e) {
-		  this.toggle_popup(e);
-		}.bind(this));
+  setup_show_observer: function() {
+    $(this.options.trigger).observe("mouseover", function(e) {
+      if (this.popup && !this.popup.visible()) {
+        this.show_popup(e);
+      }
+    }.bind(this));
   },
 
   //----------------------------------------------------------------------------
-	setup_hide_observer: function() {
-		document.observe("click", function(e) {
-			if (this.popup && this.popup.visible()) {
-			  var clicked_on = Event.findElement(e, "div");
-			  if (typeof(clicked_on) == "undefined" || clicked_on.id != this.options.target) {
+  setup_toggle_observer: function() {
+    $(this.options.trigger).observe("click", function(e) {
+      this.toggle_popup(e);
+    }.bind(this));
+  },
+
+  //----------------------------------------------------------------------------
+  setup_hide_observer: function() {
+    document.observe("click", function(e) {
+      if (this.popup && this.popup.visible()) {
+        var clicked_on = Event.findElement(e, "div");
+        if (typeof(clicked_on) == "undefined" || clicked_on.id != this.options.target) {
           this.hide_popup(e);
-				}
-			}
-		}.bind(this));
+        }
+      }
+    }.bind(this));
   },
 
   //----------------------------------------------------------------------------
-	hide_popup: function(e) {
-		this.options.before_hide(e);
-		if (!this.options.fade) {
-		  this.popup.hide();
-  		this.options.after_hide(e);
-		} else {
-		  Effect.Fade(this.popup, { duration: this.options.fade, afterFinish: function(e) { this.options.after_hide(e); }.bind(this) });
-	  }
-  },
-
-  //----------------------------------------------------------------------------
-	show_popup: function(e) {
-		e.stop();
-		if (this.options.under) {
+  show_popup: function(e) {
+    e.stop();
+    if (this.options.under) {
       var dimensions = Event.element(e).getDimensions();
       var coordinates = Event.element(e).viewportOffset();
-  		var x = coordinates[0] + "px",
-  		    y = coordinates[1] + dimensions.height + "px";
-
+      var x = coordinates[0] + "px";
+      var y = coordinates[1] + dimensions.height + "px";
       this.popup.setStyle({ left: x, top: y});
     }
     this.popup.setStyle({ zIndex: this.options.zindex });
 
-		this.options.before_show(e);
-		if (!this.options.appear) {
-		  this.popup.show();
-  		this.options.after_show(e);
-		} else {
-		  Effect.Appear(this.popup, { duration: this.options.appear, afterFinish: function(e) { this.options.after_show(e); }.bind(this) });
-	  }
-	},
-	
+    this.options.before_show(e);
+    if (!this.options.appear) {
+      this.popup.show();
+      this.options.after_show(e);
+    } else {
+      Effect.Appear(this.popup, { duration: this.options.appear, afterFinish: function(e) { this.options.after_show(e); }.bind(this) });
+    }
+  },
+
   //----------------------------------------------------------------------------
-	toggle_popup: function(e) {
-  	if (this.popup) {
-  	  this.popup.visible() ? this.hide_popup(e) : this.show_popup(e);
-		}
-	}
+  toggle_popup: function(e) {
+    if (this.popup) {
+      this.popup.visible() ? this.hide_popup(e) : this.show_popup(e);
+    }
+  },
+
+  //----------------------------------------------------------------------------
+  hide_popup: function(e) {
+    this.options.before_hide(e);
+    if (!this.options.fade) {
+      this.popup.hide();
+      this.options.after_hide(e);
+    } else {
+      Effect.Fade(this.popup, { duration: this.options.fade, afterFinish: function(e) { this.options.after_hide(e); }.bind(this) });
+    }
+  }
+
+});
+
+
+crm.Menu = Class.create({
+
+  //----------------------------------------------------------------------------
+  initialize: function() {
+    this.options = Object.extend({
+      trigger     : "menu",                   // #id of the element clicking on which triggers dropdown menu.
+      appear      : 0,                        // duration of EffectAppear or 0 for show().
+      fade        : 0,                        // duration of EffectFade or 0 for hide().
+      zindex      : 100,                      // zIndex value for the popup.
+      before_show : Prototype.emptyFunction,  // before show callback.
+      before_hide : Prototype.emptyFunction,  // before hide callback.
+      after_show  : Prototype.emptyFunction,  // after show callback.
+      after_hide  : Prototype.emptyFunction   // after hide callback.
+    }, arguments[0] || {});
+
+    this.build_menu();
+    this.setup_show_observer();
+    this.setup_hide_observer();
+  },
+
+  //----------------------------------------------------------------------------
+  build_menu: function() {
+    var ul = new Element("ul");
+    this.options.menu_items.each(function(item) {
+      var a = new Element("a", { href: "#", title: item.name });
+      if (item.on_select) {
+        a = Object.extend(a, { on_select: item.on_select });
+      }
+      var li = new Element("li").insert(a.observe("click", this.select_menu.bind(this)).update(item.name));
+      ul.insert(li);
+    }.bind(this));
+
+    this.menu = new Element("div", { className: "menu",  style: "display:none" });
+    $(document.body).insert(this.menu.insert(ul).observe("click", Event.stop));
+  },
+
+  //----------------------------------------------------------------------------
+  setup_hide_observer: function() {
+    document.observe("click", function(e) {
+      if (this.menu && this.menu.visible()) {
+        this.hide_menu();
+      }
+    }.bind(this));
+  },
+
+  //----------------------------------------------------------------------------
+  setup_show_observer: function() {
+    $(this.options.trigger).observe("click", function(e) {
+      if (this.menu && !this.menu.visible()) {
+        this.show_menu(e);
+      }
+    }.bind(this));
+  },
+
+  //----------------------------------------------------------------------------
+  hide_menu: function(e) {
+    this.options.before_hide(e);
+    if (!this.options.fade) {
+      this.menu.hide();
+      this.options.after_hide(e);
+    } else {
+      Effect.Fade(this.menu, { duration: this.options.fade, afterFinish: function(e) { this.options.after_hide(e); }.bind(this) });
+    }
+  },
+
+  //----------------------------------------------------------------------------
+  show_menu: function(e) {
+    e.stop();
+
+    var dimensions = Event.element(e).getDimensions();
+    var coordinates = Event.element(e).viewportOffset();
+    var x = coordinates[0] + "px";
+    var y = coordinates[1] + dimensions.height + "px";
+    this.menu.setStyle({ left: x, top: y}).setStyle({zIndex: this.options.zindex});
+
+    this.options.before_show(e);
+    if (!this.options.appear) {
+      this.menu.show();
+      this.options.after_show(e);
+    } else {
+      Effect.Appear(this.menu, { duration: this.options.appear, afterFinish: function(e) { this.options.after_show(e); }.bind(this) });
+    }
+    this.event = e;
+  },
+
+  //----------------------------------------------------------------------------
+  select_menu: function(e) {
+    e.stop();
+    if (e.target.on_select) {
+      this.hide_menu();
+      e.target.on_select(this.event);
+    }
+  }
 });
