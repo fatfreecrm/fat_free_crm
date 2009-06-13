14a07562882d68b1b94a901fecb14374f4787dd1
Mike Dvorkin <mike@dvorkin.net>
2009-06-13 13:52:26 -0700

    Regenerated RSpec configuration files [$ ./script/generate rspec] [#50 state:open]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-require "spec"
-require "spec/rails"
+ENV["RAILS_ENV"] ||= 'test'
+require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
+require 'spec/autorun'
+require 'spec/rails'
 require "factory_girl"
 require RAILS_ROOT + "/spec/factories"
 
@@ -25,10 +25,9 @@ Spec::Runner.configure do |config|
   config.include(SharedControllerSpecs, :type => :controller)
   #
   # == Notes
-  # 
-  # For more information take a look at Spec::Example::Configuration and Spec::Runner
+  #
+  # For more information take a look at Spec::Runner::Configuration and Spec::Runner
 end
-
 
 # Load default settings from config/settings.yml
 Factory(:default_settings)
