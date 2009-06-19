b4e34877ee14a156ef609ba45c94f1d35dca1ce4
Mike Dvorkin <mike@dvorkin.net>
2009-06-18 19:41:41 -0700

    Simplified version handling to avoid unnecessary database queries [#51 state:resolved]

--- lib/fat_free_crm.rb
+++ lib/fat_free_crm.rb
@@ -1,3 +1,4 @@
+require "fat_free_crm/version"
 require "fat_free_crm/core_ext"
 require "fat_free_crm/callback"
 require "fat_free_crm/plugin"
---
+++ lib/fat_free_crm/version.rb
@@ -0,0 +1,15 @@
+module FatFreeCRM
+  class Version
+    MAJOR = 0
+    MINOR = 9
+    TINY  = 1
+
+    def self.to_a
+      [ MAJOR, MINOR, TINY ]
+    end
+    
+    def self.to_s
+      self.to_a.join(".")
+    end
+  end
+end
