8eee15f661a174fb9ba3d1f1aaf8c25faea4b575
Mike Dvorkin <mike@dvorkin.net>
2009-07-06 19:26:02 -0700

    Replaced [mini_magick/has_image] combo with [paperclip] plugin [#58 state:open]

--- vendor/gems/mini_magick-1.2.5/lib/image_temp_file.rb
+++
@@ -1,9 +0,0 @@
-require "tempfile"
-
-module MiniMagick
-  class ImageTempFile < Tempfile
-    def make_tmpname(ext, n)
-      'mini_magick%d-%d%s' % [$$, n, ext ? ".#{ext}" : '']
-    end
-  end
-end
--- vendor/plugins/has_image/test/test_helper.rb
+++
@@ -1,8 +0,0 @@
-$LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
-
-require 'test/unit'
-require 'rubygems'
-require 'mocha'
-require 'has_image'
-
-RAILS_ROOT = File.join(File.dirname(__FILE__), '..', 'tmp')
