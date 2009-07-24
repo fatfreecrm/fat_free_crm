366c84fa78a3f3b4b696aac49e9ad11bc8a91049
Mike Dvorkin <mike@dvorkin.net>
2009-07-24 16:22:09 -0700

    Release 0.9.5

--- vendor/plugins/uses_mysql_uuid/lib/mysql_uuid_schema.rb
+++ vendor/plugins/uses_mysql_uuid/lib/mysql_uuid_schema.rb
@@ -18,11 +18,10 @@
 ActiveRecord::ConnectionAdapters::SchemaStatements.module_eval do
 
   def add_uuid_trigger(table, column = :uuid, options = { :index => true })
-    if adapter_name.downcase == "mysql"
-      if select_value("select version()").to_i >= 5
-        execute("CREATE TRIGGER #{table}_#{column} BEFORE INSERT ON #{table} FOR EACH ROW SET NEW.#{column} = UUID()")
-        add_index(table, column) if options[:index]
-      end
+    return unless @config[:adapter].downcase == "mysql" && @config[:uuid]
+    if select_value("SELECT VERSION()").to_i >= 5
+      execute("CREATE TRIGGER #{table}_#{column} BEFORE INSERT ON #{table} FOR EACH ROW SET NEW.#{column} = UUID()")
+      add_index(table, column) if options[:index]
     end
   end
 
