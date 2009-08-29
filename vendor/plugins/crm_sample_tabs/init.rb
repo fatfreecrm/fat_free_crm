3fb0257e015414a09beb392ba2a5865190110c3f
Mike Dvorkin <mike@dvorkin.net>
2009-08-29 00:15:22 -0700

    Some refactoring and more comments explaining the examples

--- vendor/plugins/crm_sample_tabs/init.rb
+++ vendor/plugins/crm_sample_tabs/init.rb
@@ -7,15 +7,25 @@ FatFreeCRM::Plugin.register(:crm_sample_tabs, initializer) do
    description "Adds new tabs to Fat Free CRM"
   dependencies :haml
 
-               # Calling tab without a block adds a new tab.
+           # The tab syntax is as follows: tab(:main | :admin, { :text => ?, :url => ? })
+           # For example, the following adds new main tab [Apples], which invokes #index in
+           # plugin's app/controllers/apples_controller.rb.
+           #-----------------------------------------------------------------------------------
            tab :main, :text => "Apples", :url => { :controller => "apples" }
-           tab :main, :text => "Oranges", :url => { :controller => "oranges" }
-           
-               # Calling tab with the block provides even more control.
+
+           # If first parameter is :main, it can be omitted. The following adds new main tab
+           # [Oranges], which invokes #index in plugin's app/controllers/oranges_controller.rb.
+           #-----------------------------------------------------------------------------------
+           tab :text => "Oranges", :url => { :controller => "oranges" }
+
+           # For ultimate control over tabs use block syntax. The following adds new Admin
+           # tab [Bananas], and moves he first tab to the end of the list. The [Bananas] tab
+           # invokes #index in app/controllers/admin/bananas_controller.rb.
+           #-----------------------------------------------------------------------------------
            tab :admin do |tabs|
              tabs << { :text => "Bananas", :url => { :controller => "bananas" } }
              tabs << tabs.shift
-             # tabs.delete_at(1)
-             # tabs.delete_if { |tab| tab[:text] == "Delete Me" }
+             # tabs.delete_at(1) # <-- Delete second tab.
+             # tabs.delete_if { |tab| tab[:text] == "Settings" } # <-- Delete [Settings] tab.
            end
 end
