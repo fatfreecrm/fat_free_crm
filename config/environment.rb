df3561e3c0920d195b4242f95839318b3b1f2090
Mike Dvorkin <mike@dvorkin.net>
2009-10-14 21:28:51 -0700

    Changed required Rails version to v2.3.4

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+# NOTE: Fat Free CRM has been tested and works with Rails 2.3.2 and 2.3.3.
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
