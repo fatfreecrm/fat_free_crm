086c1cb5f2e7f9b7c0235eba25b5736909c22e58
Mike Dvorkin <mike@dvorkin.net>
2009-12-30 22:40:47 -0800

    Fixed Ruby 1.9 compatibility issues for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -174,7 +174,7 @@ class Task < ActiveRecord::Base
     when "due_later"
       Time.zone.now.midnight + 100.years
     when "specific_time"
-      self.calendar
+      self.calendar ? Date.strptime(self.calendar, "%m/%d/%Y") : nil
     else # due_later or due_asap
       nil
     end
