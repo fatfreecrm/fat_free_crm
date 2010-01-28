ee797992d5a9969b6d493934d50835dabccc08ea
Tom Meier <ozmeier@yahoo.co.uk>
2010-01-28 11:07:26 +1100

    Fix task time zone specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,16 @@
 # Fat Free CRM
 # Copyright (C) 2008-2010 by Michael Dvorkin
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http:#www.gnu.org/licenses/>.
 #------------------------------------------------------------------------------
@@ -70,7 +70,7 @@ def login_and_assign(user_stubs = {}, session_stubs = {})
   login(user_stubs, session_stubs)
   assigns[:current_user] = @current_user
 end
- 
+
 #----------------------------------------------------------------------------
 def logout
   @current_user = nil
@@ -78,12 +78,12 @@ def logout
   Authentication.stub!(:find).and_return(nil)
 end
 alias :require_no_user :logout
-  
+
 #----------------------------------------------------------------------------
 def current_user
   @current_user
 end
- 
+
 #----------------------------------------------------------------------------
 def current_user_session
   @current_user_session
