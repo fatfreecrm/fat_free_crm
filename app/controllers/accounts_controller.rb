edbdf4c3d96e098482abfee04bd1efeea8400d8f
jose-gordo <jose.gordo@tractis.com>
2010-02-03 19:24:44 +0100

    fix

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -39,7 +39,7 @@ class AccountsController < ApplicationController
   #----------------------------------------------------------------------------
   def show
     @account = Account.my(@current_user).find(params[:id])
-    @stage = Setting.unroll(:default_access)
+    @stage = Setting.unroll(:opportunity_stage)
     @comment = Comment.new
 
     respond_to do |format|
