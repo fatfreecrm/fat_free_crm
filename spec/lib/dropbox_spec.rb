081b73134e96a00e744983538ec41bc6028971a1
Mike Dvorkin <mike@dvorkin.net>
2010-03-11 22:25:11 -0800

    Synced up dropbox specs with the actual library code

--- spec/lib/dropbox_spec.rb
+++ spec/lib/dropbox_spec.rb
@@ -11,37 +11,41 @@ describe "IMAP Dropbox" do
     Net::IMAP.stub!(:new).with(@settings[:server], @settings[:port], @settings[:ssl]).and_return(@imap)
   end
 
+  def mock_connect
+    mock_imap_connection
+    @imap.stub!(:login)
+    @imap.stub!(:select)
+    @crawler.connect
+  end
+
   #------------------------------------------------------------------------------ 
   describe "Connecting to the IMAP server" do
-    it "should connect to the IMAP server and login as user" do
-      mock_imap_connection
-      @imap.should_receive(:login).once.with(@settings[:user], @settings[:password])
-      @imap.should_not_receive(:select).with(@settings[:scan_folder])
-
-      @crawler.connect
-    end
-
-    it "should connect to the IMAP server, login as user, and select folder" do
+    it "should connect to the IMAP server and login as user, and select folder" do
       mock_imap_connection
       @imap.should_receive(:login).once.with(@settings[:user], @settings[:password])
       @imap.should_receive(:select).once.with(@settings[:scan_folder])
+      @crawler.connect
+    end
 
-      @crawler.connect(true)
+    it "should connect to the IMAP server, login as user, but not select folder when requested so" do
+      mock_imap_connection
+      @imap.should_receive(:login).once.with(@settings[:user], @settings[:password])
+      @imap.should_not_receive(:select).with(@settings[:scan_folder])
+      @crawler.connect(false)
     end
 
     it "should raise the error if connection fails" do
       Net::IMAP.should_receive(:new).and_raise(SocketError) # No mocks this time! :-)
-      @crawler.connect
+      lambda { @crawler.connect }.should raise_error SystemExit
     end
   end
 
   #------------------------------------------------------------------------------ 
   describe "Disconnecting from the IMAP server" do
     it "should logout and diconnect" do
-      mock_imap_connection
-      @crawler.connect
-      @imap.should_receive(:logout)#.once
-      @imap.should_receive(:disconnect)#.once
+      mock_connect
+      @imap.should_receive(:logout).once
+      @imap.should_receive(:disconnect).once
       @crawler.disconnect
     end
   end
@@ -49,9 +53,8 @@ describe "IMAP Dropbox" do
   #------------------------------------------------------------------------------ 
   describe "Discarding a message" do
     before(:each) do
-      mock_imap_connection
+      mock_connect
       @crawler.instance_variable_set("@current_uid", @current_uid = mock)
-      @crawler.connect
     end
 
     it "should copy message to invalid folder if it's set and flag the message as deleted" do
@@ -73,23 +76,22 @@ describe "IMAP Dropbox" do
   #------------------------------------------------------------------------------     
   describe "Arciving a message" do
     before(:each) do
-      mock_imap_connection
-      @uid = mock
-      @crawler.connect
+      mock_connect
+      @crawler.instance_variable_set("@current_uid", @current_uid = mock)
     end
 
     it "should copy message to archive folder if it's set and flag the message as seen" do
       @settings[:move_to_folder] = "processed"
-      @imap.should_receive(:uid_copy).once.with(@uid, @settings[:move_to_folder])
-      @imap.should_receive(:uid_store).once.with(@uid, "+FLAGS", [:Seen])      
-      @crawler.archive(@uid)
+      @imap.should_receive(:uid_copy).once.with(@current_uid, @settings[:move_to_folder])
+      @imap.should_receive(:uid_store).once.with(@current_uid, "+FLAGS", [:Seen])
+      @crawler.archive
     end
 
     it "should not copy message to archive folder if it's not set and flag the message as seen" do
       @settings[:move_to_folder] = nil
       @imap.should_not_receive(:uid_copy)
-      @imap.should_receive(:uid_store).once.with(@uid, "+FLAGS", [:Seen])
-      @crawler.archive(@uid)
+      @imap.should_receive(:uid_store).once.with(@current_uid, "+FLAGS", [:Seen])
+      @crawler.archive
     end
   end
 
