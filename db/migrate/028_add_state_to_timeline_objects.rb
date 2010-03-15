c0e1b3d3e3936de29ced511787bcaca6335b114f
Jose Luis Gordo <jose.gordo@tractis.com>
2010-03-15 17:08:33 +0100

    Implemented dropbox ack and lib bugfixing

--- db/migrate/028_add_state_to_timeline_objects.rb
+++ db/migrate/028_add_state_to_timeline_objects.rb
@@ -1,7 +1,7 @@
 class AddStateToTimelineObjects < ActiveRecord::Migration
   def self.up
-    add_column :comments, :state, :string
-    add_column :emails, :state, :string
+    add_column :comments, :state, :string, :default => "collapsed"
+    add_column :emails, :state, :string, :default => "collapsed"
   end
 
   def self.down
