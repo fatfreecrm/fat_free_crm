272409f37e8df56a09cb44370a9b93079d796704
Ralf Ebert <info@ralfebert.de>
2010-08-04 11:29:46 +0200

    unbundled will_paginate and updated from 2.3.8 to latest gem version 2.3.14

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem 'will_paginate', :version => '2.3.14' #, :source => 'http://gemcutter.org'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
