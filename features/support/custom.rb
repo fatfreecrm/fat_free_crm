e07b35d96e73cfd9ce2e032856e62cd0ea02ca9b
Nathan B <nathan.f77@gmail.com>
2010-08-12 11:40:23 +0800

    update vendor/plugins/crm_tags

--- features/support/custom.rb
+++ features/support/custom.rb
@@ -1,10 +1,8 @@
 Spork.prefork do
   require "factory_girl"
   require RAILS_ROOT + "/spec/factories"
-
-  require "selenium-webdriver"
-  Selenium::WebDriver.for :chrome
 end
 
 Spork.each_run do
 end
+
