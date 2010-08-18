65bb9c1bf281442863c71c4ebbe1e2ef22c94452
Mike Dvorkin <mike@dvorkin.net>
2010-08-17 21:36:55 -0700

    Properly set the account value when attaching a contact to the account [#165 state:resolved]

--- app/models/account.rb
+++ app/models/account.rb
@@ -81,7 +81,12 @@ class Account < ActiveRecord::Base
   #----------------------------------------------------------------------------
   def attach!(attachment)
     unless self.send("#{attachment.class.name.downcase}_ids").include?(attachment.id)
-      self.send(attachment.class.name.tableize) << attachment
+      if attachment.is_a?(Contact)
+        attachment.account = self
+        [ attachment ]
+      else
+        self.send(attachment.class.name.tableize) << attachment
+      end
     end
   end
 
