35617908c5aac90bd2c0ad556a6408a4f5643ee0
Ben Tillman <bstillman@crossroadsint.org>
2010-08-20 10:55:12 +0800

    Fixed depricated action mailer api call

--- app/models/user.rb
+++ app/models/user.rb
@@ -118,7 +118,7 @@ class User < ActiveRecord::Base
   #----------------------------------------------------------------------------
   def deliver_password_reset_instructions!
     reset_perishable_token!
-    Notifier.deliver_password_reset_instructions(self)
+    Notifier.password_reset_instructions(self).deliver
   end
 
 
