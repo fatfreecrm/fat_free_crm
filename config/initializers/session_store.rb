e6b38a651447cec53431acfc7b2349510a8961a2
Ben Tillman <bstillman@crossroadsint.org>
2010-08-26 10:45:30 +0800

    Fixed session store name and moved README

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-FatFreeCrm::Application.config.session_store :cookie_store, :key => '_crossroads_crm_session'
+FatFreeCrm::Application.config.session_store :cookie_store, :key => '_fat_free_crm_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
