3c9efdb37711d9705c8d20250b639d47727a0d6a
Steve Kenworthy <swkenworthy@crossroadsint.org>
2010-08-27 17:05:54 +0800

    Tidyied up bamboo script

--- script/build_bamboo.sh
+++ script/build_bamboo.sh
@@ -1,39 +1,26 @@
-##
-## Install required pacakges
-##
-
+# Install required pancakes
 yum --quiet -y install ruby ruby-devel gcc rubygems
-
-##
-## Install the required Gems
-##
-
-/usr/bin/gem install rake ci_reporter --no-rdoc --no-ri
+/usr/bin/gem install rake -v=0.8.7 --no-rdoc --no-ri
 /usr/bin/gem install rails -v=2.3.8 --no-rdoc --no-ri
+/usr/bin/gem install ci_reporter -v=1.6.2 --no-rdoc --no-ri
 
-##
-## Create Database Configuration File
-##
-
+# Create Database Configuration File
 cat << EOF > config/database.yml
 test:
   adapter: mysql
   encoding: utf8
-  database: fat_free_crm_test
+  database: crm_test
   username: root
   password: root
   socket: /var/lib/mysql/mysql.sock
 EOF
 
-##
-## Cleanup after the previous run.
-##
-
+# Cleanup after the previous run.
 rm -rf ./spec/reports/*
 
 RAILS_ENV=test rake db:create
 RAILS_ENV=test rake gems:install
 RAILS_ENV=test rake db:migrate
 RAILS_ENV=test rake db:migrate:plugins
-./script/spec --require /usr/lib/ruby/gems/1.8/gems/ci_reporter-1.6.2/lib/ci/reporter/rake/rspec_loader --format CI::Reporter::RSpec spec/
+./script/spec --require `ruby -r 'rubygems' -e 'puts Gem.path.last'`/gems/ci_reporter-1.6.2/lib/ci/reporter/rake/rspec_loader --format CI::Reporter::RSpec spec/
 RAILS_ENV=test rake db:drop
