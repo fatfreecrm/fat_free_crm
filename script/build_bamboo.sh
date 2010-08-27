466651ef0f185e8821c03a6485bdee76e1e14ef9
Steve Kenworthy <swkenworthy@crossroadsint.org>
2010-08-27 18:15:01 +0800

    Better version detection for gems. Including submodules

--- script/build_bamboo.sh
+++ script/build_bamboo.sh
@@ -1,8 +1,8 @@
 # Install required pancakes
 yum --quiet -y install ruby ruby-devel gcc rubygems
-if ! (gem list | grep "rake (0.8.7)"); then gem install rake -v=0.8.7 --no-rdoc --no-ri; fi;
-if ! (gem list | grep "rails (2.3.8)"); then gem install rails -v=2.3.8 --no-rdoc --no-ri; fi;
-if ! (gem list | grep "ci_reporter (1.6.2)"); then gem install ci_reporter -v=1.6.2 --no-rdoc --no-ri; fi;
+if ! (gem list | grep "rake" | grep "0.8.7"); then gem install rake -v=0.8.7 --no-rdoc --no-ri; fi;
+if ! (gem list | grep "rails" | grep "2.3.8"); then gem install rails -v=2.3.8 --no-rdoc --no-ri; fi;
+if ! (gem list | grep "ci_reporter" | grep "1.6.2"); then gem install ci_reporter -v=1.6.2 --no-rdoc --no-ri; fi;
 
 # Create Database Configuration File
 cat << EOF > config/database.yml
@@ -16,12 +16,15 @@ test:
 EOF
 
 # Cleanup after the previous run.
-ll ./spec/reports/ # stub to tell us if old files are still there REMOVE
+ls ./spec/reports/ # stub to tell us if old files are still there REMOVE
 rm -rf ./spec/reports/*
 
-RAILS_ENV=test rake db:create
+git submodule init
+git submodule update
 RAILS_ENV=test rake gems:install
+RAILS_ENV=test rake db:create
 RAILS_ENV=test rake db:migrate
 RAILS_ENV=test rake db:migrate:plugins
+echo "Running tests..."
 ./script/spec --require `ruby -r 'rubygems' -e 'puts Gem.path.last'`/gems/ci_reporter-1.6.2/lib/ci/reporter/rake/rspec_loader --format CI::Reporter::RSpec spec/
 RAILS_ENV=test rake db:drop
