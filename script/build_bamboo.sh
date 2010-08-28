b5905e4b2b18988b19f385c16a81dbe90f651a39
Steve Kenworthy <swkenworthy@crossroadsint.org>
2010-08-28 14:50:35 +0800

    More debug code

--- script/build_bamboo.sh
+++ script/build_bamboo.sh
@@ -18,19 +18,22 @@ EOF
 # Pull submodules from github read-only url. Prevents needing to authenticate this machine.
 
 ls -l vendor/plugins
+cat "ls crm_super_tags"
 ls -l vendor/plugins/crm_super_tags
 cat .gitmodules
 sed -i s,git@github.com:,http://github.com/,g .gitmodules
 cat .gitmodules
 echo "submodule sync"
-git submodule sync 2&>1;
+git submodule sync
 echo "submodule update"
-git submodule update --init 2&>1;
+git submodule update --init
+cat "ls crm_super_tags"
+ls -l vendor/plugins/crm_super_tags
 
-RAILS_ENV=test rake gems:install
-RAILS_ENV=test rake db:create
-RAILS_ENV=test rake db:migrate
-RAILS_ENV=test rake db:migrate:plugins
-./script/spec --require `ruby -r 'rubygems' -e 'puts Gem.path.last'`/gems/ci_reporter-1.6.2/lib/ci/reporter/rake/rspec_loader --format CI::Reporter::RSpec spec/
-# run submodule tests
-RAILS_ENV=test rake db:drop
+#RAILS_ENV=test rake gems:install
+#RAILS_ENV=test rake db:create
+#RAILS_ENV=test rake db:migrate
+#RAILS_ENV=test rake db:migrate:plugins
+#./script/spec --require `ruby -r 'rubygems' -e 'puts Gem.path.last'`/gems/ci_reporter-1.6.2/lib/ci/reporter/rake/rspec_loader --format CI::Reporter::RSpec spec/
+## run submodule tests
+#RAILS_ENV=test rake db:drop
