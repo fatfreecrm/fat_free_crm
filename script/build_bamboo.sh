b307339034e876f5ae438845fd443528596206be
Steve Kenworthy <swkenworthy@crossroadsint.org>
2010-08-28 12:30:33 +0800

    Add github public key to known_hosts. This enables us to fetch the submodules.

--- script/build_bamboo.sh
+++ script/build_bamboo.sh
@@ -15,16 +15,15 @@ test:
   socket: /var/lib/mysql/mysql.sock
 EOF
 
-# Cleanup after the previous run.
-ls ./spec/reports/ # stub to tell us if old files are still there REMOVE
-rm -rf ./spec/reports/*
-
+# Add github key to known_hosts so we can fetch submodules
+touch ~/.ssh/known_hosts
+if ! (grep "github.com" ~/.ssh/known_hosts); then echo "github.com,207.97.227.239 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> ~/.ssh/known_hosts; fi;
 git submodule init
 git submodule update
+
 RAILS_ENV=test rake gems:install
 RAILS_ENV=test rake db:create
 RAILS_ENV=test rake db:migrate
 RAILS_ENV=test rake db:migrate:plugins
-echo "Running tests..."
 ./script/spec --require `ruby -r 'rubygems' -e 'puts Gem.path.last'`/gems/ci_reporter-1.6.2/lib/ci/reporter/rake/rspec_loader --format CI::Reporter::RSpec spec/
 RAILS_ENV=test rake db:drop
