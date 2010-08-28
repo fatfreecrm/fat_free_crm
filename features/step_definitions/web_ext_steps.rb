98e9de7b6f5f8ab6f8bb6419fb813b885191e969
Nathan B <nathan.f77@gmail.com>
2010-08-28 12:59:27 +0800

    Added css selector for event simulations

--- features/step_definitions/web_ext_steps.rb
+++ features/step_definitions/web_ext_steps.rb
@@ -8,6 +8,15 @@ end
 Then /^I fire the "([^"]*)" event on "([^"]*)"$/ do |event, id|
   begin
     Capybara.current_session.driver.browser.execute_script("$('#{id}').simulate('#{event}');")
+    sleep 1
+  rescue Capybara::NotSupportedByDriverError
+  end
+end
+
+Then /^I fire the "([^"]*)" event on css selector "([^"]*)"$/ do |event, selector|
+  begin
+    Capybara.current_session.driver.browser.execute_script("$$('#{selector}').first().simulate('#{event}');")
+    sleep 1
   rescue Capybara::NotSupportedByDriverError
   end
 end
