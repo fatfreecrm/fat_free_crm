d30e0359645be30ad379704dd54f1e1350eab665
Nathan B <nathan.f77@gmail.com>
2010-08-31 11:54:39 +0800

    Added awesome print to test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,3 +31,10 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
+
+# Optionally load 'awesome_print' for debugging in development mode.
+begin
+  require 'ap'
+rescue LoadError
+end
+
