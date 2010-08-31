b36448c6944c397928e44e0e19e92e606918fef6
Steve Kenworthy <swkenworthy@crossroadsint.org>
2010-08-31 14:27:27 +0800

    Dependencies for crm_super_tags

--- script/build_bamboo.sh
+++ script/build_bamboo.sh
@@ -3,6 +3,8 @@ yum --quiet -y install ruby ruby-devel gcc rubygems
 if ! (gem list | grep "rake" | grep "0.8.7"); then gem install rake -v=0.8.7 --no-rdoc --no-ri; fi;
 if ! (gem list | grep "rails" | grep "2.3.8"); then gem install rails -v=2.3.8 --no-rdoc --no-ri; fi;
 if ! (gem list | grep "ci_reporter" | grep "1.6.2"); then gem install ci_reporter -v=1.6.2 --no-rdoc --no-ri; fi;
+# crm_super_tags dependency
+if ! (gem list | grep "database_cleaner" | grep "0.4.3"); then gem install database_cleaner -v=0.4.3 --no-rdoc --no-ri; fi;
 
 # Create Database Configuration File
 cat << EOF > config/database.yml
@@ -20,21 +22,21 @@ sed -i s,git@github.com:,http://github.com/,g .git/config
 git submodule update
 
 # fat free crm tests
-RAILS_ENV=test rake gems:install
 RAILS_ENV=test rake db:create
+RAILS_ENV=test rake gems:install
 RAILS_ENV=test rake db:migrate
 RAILS_ENV=test rake db:migrate:plugins
-./script/spec --require `ruby -r 'rubygems' -e 'puts Gem.path.last'`/gems/ci_reporter-1.6.2/lib/ci/reporter/rake/rspec_loader --format CI::Reporter::RSpec spec/
+#./script/spec --require `ruby -r 'rubygems' -e 'puts Gem.path.last'`/gems/ci_reporter-1.6.2/lib/ci/reporter/rake/rspec_loader --format CI::Reporter::RSpec spec/
 
 # crm_super_tags tests
 cd vendor/plugins/crm_super_tags
 spec --require `ruby -r 'rubygems' -e 'puts Gem.path.last'`/gems/ci_reporter-1.6.2/lib/ci/reporter/rake/rspec_loader --format CI::Reporter::RSpec spec/
-# cucumbers
+# cucumbers need cucumber env
 
 # crm_merge_contacts tests
 #specs
 #cd ../../crm_merge_contacts
-# cucumbers
+# cucumbers need cucumber env
 
 # drop database
 RAILS_ENV=test rake db:drop
