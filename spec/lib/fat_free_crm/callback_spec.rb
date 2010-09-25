e49161691f7d738c62f591a87160189a057c3bb4
Nathan B <nathan.f77@gmail.com>
2010-09-25 12:32:02 +0800

    * Heavily refactored and reworked callback extension. View hooks are now treated as a separate entity, distinct from controller and legacy view hooks. However, they both still use the same @@responder variable to store the classes that implement a given hook. View hooks are now stored as Procs in a hash instead of being defined as methods on the class. This allows a Callback subclass to chain data operations for the same hook.   * Improved documentation and tidied up code.   * Added specs for chaining view hook operations, created new CallbackSpecHelper module to contain helper classes and methods.

--- spec/lib/fat_free_crm/callback_spec.rb
+++ spec/lib/fat_free_crm/callback_spec.rb
@@ -1,30 +1,55 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-class TestCallback < FatFreeCRM::Callback::Base
-  insert_before :test_before do |view, context|
-    "BEFORE-"
+module CallbackSpecHelper
+  class TestCallback < FatFreeCRM::Callback::Base
+    # --------------------------------------------
+    insert_before :test_before do |view, context|
+      "BEFORE-"
+    end
+    # --------------------------------------------
+    insert_after :test_after do |view, context|
+      "-AFTER"
+    end
+    # --------------------------------------------
+    replace :test_replace do |view, context|
+      "REPLACE"
+    end
+    # --------------------------------------------
+    remove :test_remove
+    # --------------------------------------------
+    insert_before :test_before_and_after do |view, context|
+      "BEFORE-"
+    end
+    insert_after :test_before_and_after do |view, context|
+      "-AFTER"
+    end
+    # --------------------------------------------
+    insert_before :test_before_and_after_with_replace do |view, context|
+      "BEFORE-"
+    end
+    insert_after :test_before_and_after_with_replace do |view, context|
+      "-AFTER"
+    end
+    replace :test_before_and_after_with_replace do |view, context|
+      "REPLACED"
+    end
+    # --------------------------------------------
+    def test_legacy(view, context={})
+      "LEGACY"
+    end
   end
-  insert_after :test_after do |view, context|
-    "-AFTER"
+
+  def test_hook(position)
+    Haml::Engine.new(%Q^
+= hook("test_#{position}".to_sym, ActionView::Base.new) do
+  BLOCK^).render.gsub("\n",'')
   end
-  replace :test_replace do |view, context|
-    "REPLACE"
-  end
-  remove :test_remove
   
-  def test_legacy(view, context={})
-    "LEGACY"
-  end
 end
 
 include FatFreeCRM::Callback::Helper
 include ActionView::Helpers::CaptureHelper
-
-def test_hook(position)
-  Haml::Engine.new(%Q^
-= hook("test_#{position}".to_sym, ActionView::Base.new) do
-  BLOCK^).render.gsub("\n",'')
-end
+include CallbackSpecHelper
 
 describe FatFreeCRM::Callback do 
   it "should insert content before the block" do
@@ -42,10 +67,17 @@ describe FatFreeCRM::Callback do
   it "should remove the block" do
     test_hook(:remove).should == ""
   end
+
+  it "should be able to insert content before and after the block" do
+    test_hook(:before_and_after).should == "BEFORE-BLOCK-AFTER"
+  end
+
+  it "should be able to insert content before and after a replaced block" do
+    test_hook(:before_and_after_with_replace).should == "BEFORE-REPLACED-AFTER"
+  end
   
   it "should still work for legacy hooks" do
-  Haml::Engine.new(%Q^
-= hook(:test_legacy, ActionView::Base.new)
-  ^).render.gsub("\n",'').should == "LEGACY"
+    Haml::Engine.new("= hook(:test_legacy, ActionView::Base.new)").render.
+      gsub("\n",'').should == "LEGACY"
   end
 end
