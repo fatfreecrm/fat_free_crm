cf8c40b99b5d759eede0c6ebd250218af97f1cab
Nathan B <nathan.f77@gmail.com>
2010-09-30 17:59:45 +0800

    Removed event.js.simulate code from cuke custom.rb

--- features/support/custom.rb
+++ features/support/custom.rb
@@ -46,15 +46,6 @@ end
 # Default timeout should be longer since this is an AJAX based application.
 Capybara.default_wait_time = 7
 
-
-# Add event.simulate.js to cucumber environment,
-# so that we can simulate events such as mouseclicks.
-class EventSimulateViewHooks < FatFreeCRM::Callback::Base
-  def javascript_includes(view, context = {})
-    view.javascript_include_tag "event.simulate.js"
-  end
-end
-
 # Cancel any activity logging for Comment model (breaks cucumber tests)
 Comment.class_eval do
   def log_activity; end
