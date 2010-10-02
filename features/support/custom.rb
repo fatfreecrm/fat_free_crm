8896185a90656ea78a8910a8972cc8a3909ed07f
Nathan B <nathan.f77@gmail.com>
2010-10-02 16:02:51 +0800

    Moved all test override logic into plugin

--- features/support/custom.rb
+++ features/support/custom.rb
@@ -18,30 +18,8 @@ if ENV['HEADLESS'] == 'true'
   puts "Running in Headless mode. Display #{HEADLESS_DISPLAY}"
 end
 
-
-def skip_scenario?(scenario, file, name)
-  # Skip the scenario if required by a loaded plugin.
-  (file == File.basename(scenario.feature.file)) and (name.nil? || name == scenario.name)
-end
-
-# Plugins need to be able to inform the main cucumber suite when they have
-# intentionally broken a scenario, so that the scenario can be skipped
-# and overwritten safely.
-Before do |scenario|
-  FatFreeCRM::Cucumber.expected_failures.each do |plugin, file, name|  
-    if skip_scenario?(scenario, file, name)
-      print "/!" # Print at start of skipped steps
-      scenario.instance_variable_get("@steps").each do |step|
-        step.skip_invoke!
-      end
-    end  
-  end 
-end
-After do |scenario|
-  FatFreeCRM::Cucumber.expected_failures.each do |plugin, file, name|  
-    print "(#{plugin.to_s})!/" if skip_scenario?(scenario, file, name) # Print at end of skipped steps
-  end 
-end
+# Require plugin.rb support files from each plugin.
+Dir.glob("#{Rails.root}/vendor/plugins/**/support/plugin.rb").each {|f| require f }
 
 # Default timeout should be longer since this is an AJAX based application.
 Capybara.default_wait_time = 7
