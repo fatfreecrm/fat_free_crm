190abac0cc432e22b5f814ac837c1721bfe8e655
Mike Dvorkin <mike@dvorkin.net>
2010-10-05 20:36:46 -0700

    Fixed undefined html_safe method

--- lib/fat_free_crm/exceptions.rb
+++ lib/fat_free_crm/exceptions.rb
@@ -27,7 +27,7 @@ class ActionController::Base
   private
 
   def render_fat_free_crm_exception(exception)
-    logger.error(exception)
-    render :layout => false, :template => "/layouts/500.html.haml", :status => 500, :locals => { :exception => exception.html_safe }
+    logger.error exception.inspect
+    render :layout => false, :template => "/layouts/500.html.haml", :status => 500, :locals => { :exception => exception.to_s.html_safe }
   end
 end
