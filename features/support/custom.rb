e45ff9011e8dea91a142df4463649d1f051a526d
Nathan B <nathan.f77@gmail.com>
2010-10-07 13:53:04 +0800

    removed duplicate headless initialization, added database clean before running cucumbers.

--- features/support/custom.rb
+++ features/support/custom.rb
@@ -1,23 +1,6 @@
 require "factory_girl"
 require "#{::Rails.root}/spec/factories"
 
-#
-# headless
-# sudo apt-get install xvfb
-# OR yum install xorg-x11-server-Xvfb.x86_64
-# gem install headless
-#
-if ENV['HEADLESS'] == 'true'
-  require 'headless'
-  headless = Headless.new
-  headless.start
-  HEADLESS_DISPLAY = headless.display
-  at_exit do
-    headless.destroy
-  end
-  puts "Running in Headless mode. Display #{HEADLESS_DISPLAY}"
-end
-
 # Require plugin.rb support files from each plugin.
 Dir.glob("#{Rails.root}/vendor/plugins/**/support/plugin.rb").each {|f| require f }
 
@@ -28,4 +11,7 @@ Capybara.default_wait_time = 7
 Comment.class_eval do
   def log_activity; end
 end
+
+# Make sure the database is clean and shiny before we start testing
+DatabaseCleaner.clean
 
