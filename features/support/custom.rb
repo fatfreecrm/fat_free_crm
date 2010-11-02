a0d93619834645d4ae1cd38c0fa543d9b4d25b4d
Ben Tillman <bstillman@crossroadsint.org>
2010-11-02 20:58:34 +0800

    Realised ActiveRecord::Base is not loaded in custom

--- features/support/custom.rb
+++ features/support/custom.rb
@@ -1,23 +1,21 @@
 require "factory_girl"
 require "#{::Rails.root}/spec/factories"
 
-if defined?(ActiveRecord::Base)
-  begin
-    require 'database_cleaner'
+begin
+  require 'database_cleaner'
 
-    # Restart identity fix for postgresql
-    ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
-      def truncate_table(table_name)
-        execute("TRUNCATE TABLE #{quote_table_name(table_name)} RESTART IDENTITY #{cascade};")
-      end
+  # Restart identity fix for postgresql
+  ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
+    def truncate_table(table_name)
+      execute("TRUNCATE TABLE #{quote_table_name(table_name)} RESTART IDENTITY #{cascade};")
     end
-    DatabaseCleaner.app_root = ::Rails.root
-    DatabaseCleaner.strategy = :truncation, {:except => ['settings']}
-
-    # Make sure the database is clean and shiny before we start testing
-    DatabaseCleaner.clean
-  rescue LoadError => ignore_if_database_cleaner_not_present
   end
+  DatabaseCleaner.app_root = ::Rails.root
+  DatabaseCleaner.strategy = :truncation, {:except => ['settings']}
+
+  # Make sure the database is clean and shiny before we start testing
+  DatabaseCleaner.clean
+rescue LoadError => ignore_if_database_cleaner_not_present
 end
 
 # Require plugin.rb support files from each plugin.
