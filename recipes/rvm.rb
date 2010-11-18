ea868435293fe1d9f2ad300abf78ff74a984a33d
Ben Tillman <bstillman@crossroadsint.org>
2010-11-18 10:53:23 +0800

    Added openssl-devel to rvm recipe

--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -4,12 +4,12 @@ namespace :rvm do
   task :install, :roles => :web do
     install_deps
 
-    #~ run "if ! (which rvm); then curl -L http://bit.ly/rvm-install-system-wide | bash; fi"
+    run "if ! (which rvm); then sudo curl -L http://bit.ly/rvm-install-system-wide | sudo bash; fi"
     run "if ! (rvm list | grep #{rvm_ruby_string}); then sudo rvm install #{rvm_ruby_string}; fi"
   end
 
   task :install_deps, :roles => :web do
-    yum.install( {:base => %w(curl git gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel)}, :stable )
+    yum.install( {:base => %w(curl git gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel)}, :stable )
   end
 
 end
