ae5bc306f485e38b178775a0c6728a315ad96593
Nathan B <nathan.f77@gmail.com>
2010-12-10 15:12:26 +0800

    Fixed tasks specs by ensuring task_calendar_with_time is set to false

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -24,6 +24,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Task do
 
+  before(:all) do
+    Setting.task_calendar_with_time = false
+  end
+
   before(:each) do
     login
   end
