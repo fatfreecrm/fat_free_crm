45da3702666f7ab1845d895735d3a772f5a8132e
Steve Kenworthy <swkenworthy@crossroadsint.org>
2010-12-10 15:57:40 +0800

    Speeding up time for testing purposes.

--- config/schedule.rb
+++ config/schedule.rb
@@ -21,6 +21,6 @@
 
 job_type :rvm_rake, "source /usr/local/lib/rvm && cd :path && RAILS_ENV=:environment rake :task :output"
 
-every 15.minutes do
+every 3.minutes do
   rvm_rake "crm:dropbox:run", :output => "log/dropbox_cron.log"
 end
