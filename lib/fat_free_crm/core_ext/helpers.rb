0ccf4a8fef25997ab333f796788bc6cd05e7bc92
Mike Dvorkin <mike@dvorkin.net>
2010-12-11 20:58:21 -0800

    Small tweaks

--- lib/fat_free_crm/core_ext/helpers.rb
+++ lib/fat_free_crm/core_ext/helpers.rb
@@ -15,14 +15,14 @@
 # along with this program.  If not, see <http:#www.gnu.org/licenses/>.
 #------------------------------------------------------------------------------
 
-class ActionController::Base
-
-  # Remove helpers residing in subdirectories from the list of application
-  # helpers.  Basically we don't want helpers in app/helpers/admin/* to
-  # override the ones in app/helpers/*.
-  #----------------------------------------------------------------------------
-  def self.all_application_helpers
-    super.delete_if { |helper| helper.include?(File::SEPARATOR) && !Rails.env.test? }
+unless Rails.env.test?
+  class ActionController::Base
+    # Remove helpers residing in subdirectories from the list of application
+    # helpers.  Basically we don't want helpers in app/helpers/admin/* to
+    # override the ones in app/helpers/*.
+    #----------------------------------------------------------------------------
+    def self.all_application_helpers
+      super.delete_if { |helper| helper.include?(File::SEPARATOR) }
+    end
   end
-
 end
