9812e469c268a524359e11e50a61dd73e8a4fd68
Nathan B <nathan.f77@gmail.com>
2010-12-14 16:44:37 +0800

    Adjusted deploy script to use settings.yml.example in crm_crossroads

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,7 @@ namespace :crm do
 
   desc "Load crm settings"
   task :settings do
-    run "if [ -f #{shared_path}/settings.sed ]; then sed -i -f #{shared_path}/settings.sed #{current_path}/vendor/plugins/crm_crossroads/config/settings.yml; fi"
+    run "if [ -f #{shared_path}/settings.sed ]; then cp -f #{current_path}/vendor/plugins/crm_crossroads/config/settings.yml.example #{current_path}/vendor/plugins/crm_crossroads/config/settings.yml; sed -i -f #{shared_path}/settings.sed #{current_path}/vendor/plugins/crm_crossroads/config/settings.yml; fi"
     run "cd #{current_path} && bundle exec rake crm:settings:load PLUGIN=crm_crossroads RAILS_ENV=production"
   end
 
