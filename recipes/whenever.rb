d1e3521ef6424cfb360f2de3085b477a8b467fe2
Steve Kenworthy <swkenworthy@crossroadsint.org>
2010-12-21 11:26:49 +0800

    Install to ec2 user crontab

--- recipes/whenever.rb
+++ recipes/whenever.rb
@@ -7,6 +7,6 @@ after "deploy:symlink", "deploy:update_crontab"
 namespace :deploy do
   desc "Update the crontab file"
   task :update_crontab, :roles => :db do
-    run "source /usr/local/lib/rvm && cd #{current_path} && bundle exec whenever --update-crontab #{application}"
+    run "cd #{current_path} && bundle exec whenever --update-crontab #{application}"
   end
 end
