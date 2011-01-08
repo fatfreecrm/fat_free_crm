55692b7c46578be101164f501070cf8ba2da34a1
Nathan B <nathan.f77@gmail.com>
2011-01-08 13:05:42 +0800

    removed 'require ap' from tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #------------------------------------------------------------------------------
-require 'ap'
 class TasksController < ApplicationController
   before_filter :require_user
   before_filter :auto_complete, :only => :auto_complete
