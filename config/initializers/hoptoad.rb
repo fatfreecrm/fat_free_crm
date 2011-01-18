7d00d0b2bc4e7a8ccec113e496d8b77d4c2e4844
Ben Tillman <bstillman@crossroadsint.org>
2011-01-18 12:57:40 +0800

    Changed notifier to be enviroment aware

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,4 +1,5 @@
 HoptoadNotifier.configure do |config|
   config.api_key = 'a95a20784fd5bce6e8aefba981fd7a99'
   config.js_notifier = true
+  config.environment_name = (ENV['HOSTNAME'] =~ /preview/ ? 'preview' : 'live')
 end
