50b32d31495d0570eecd9ed241a942b28dd53ece
Nathan B <nathan.f77@gmail.com>
2011-02-12 13:47:07 +0800

    Reconfigured Sass so that plugins could override css

--- config/initializers/fat_free_crm.rb
+++ config/initializers/fat_free_crm.rb
@@ -1,5 +1,6 @@
 require "fat_free_crm"
 
 #---------------------------------------------------------------------
-Sass::Plugin.options[:template_location] = File.join(Rails.root, "app/stylesheets")
+Sass::Plugin.add_template_location File.join(Rails.root, "app/stylesheets")
 Sass::Plugin.options[:css_location] = File.join(Rails.root, "public/stylesheets")
+
