4db708e35458cb56fa09ca7e3b928284b9306529
Alex Tomlins <alex.tomlins@unboxedconsulting.com>
2011-02-22 10:00:32 +0000

    Remove redundant gemcutter sources

--- config/environment.rb
+++ config/environment.rb
@@ -23,8 +23,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'rack', :version => "1.1.0"
-  config.gem 'will_paginate', :version => '2.3.14', :source => 'http://gemcutter.org'
-  config.gem 'faker', :version => '0.3.1', :source => 'http://gemcutter.org'
+  config.gem 'will_paginate', :version => '2.3.14'
+  config.gem 'faker', :version => '0.3.1'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
