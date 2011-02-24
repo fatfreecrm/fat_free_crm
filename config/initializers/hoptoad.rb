971c175528ac30282a128f3653d4dff12926c245
Nathan B <nathan.f77@gmail.com>
2011-02-24 12:43:23 +0800

    Made hoptoad deployment notification 80% faster

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,4 +1,4 @@
 HoptoadNotifier.configure do |config|
   config.api_key = 'a95a20784fd5bce6e8aefba981fd7a99'
-  config.environment_name = 'preview' if Rails.env == 'production' and ENV['HOSTNAME'] =~ /preview/
+  config.environment_name = 'preview' if ENV['TO'] == 'production' and ENV['HOSTNAME'] =~ /preview/
 end
