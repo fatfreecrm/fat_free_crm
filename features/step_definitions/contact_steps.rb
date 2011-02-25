3742ef4f63a7cb8a7984604e2d64f4333faca52a
Nathan B <nathan.f77@gmail.com>
2011-02-25 17:26:03 +0800

    Made some backwards-compatible changes to templates that allow comment partials to be used multiple times on the same page (for example, on an index of contacts)

--- features/step_definitions/contact_steps.rb
+++ features/step_definitions/contact_steps.rb
@@ -7,3 +7,13 @@ end
 Given /^an contact with params:$/ do |params|
   @contact = Factory(:contact, params.rows_hash)
 end
+
+And /^the contact is tagged with "([^"]*)"$/ do |tag_name|
+  @contact.update_attribute(:tag_list, tag_name)
+end
+
+And /^the contact has a note with the text "([^"]+)"$/ do |note|
+  @comment = Factory(:comment, :commentable => @contact,
+                               :comment     => note)
+end
+
