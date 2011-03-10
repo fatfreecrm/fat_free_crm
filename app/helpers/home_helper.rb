48d6db6543b86e55e78d79392e0982c5fb94bf2d
Nathan B <nathan.f77@gmail.com>
2011-03-10 13:02:14 +0800

    You cannot translate strings and then freeze them in a constant. Strings must be translated at runtime.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,16 +1,16 @@
 # Fat Free CRM
 # Copyright (C) 2008-2010 by Michael Dvorkin
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #------------------------------------------------------------------------------
@@ -18,14 +18,14 @@
 module HomeHelper
   def sort_by_assets
     Activity::ASSETS.map do |key, value|
-      %Q[{ name: "#{value}", on_select: function() { #{redraw(:asset, [ key, value.downcase ], url_for(:action => :redraw))} } }]
+      %Q[{ name: "#{t(value)}", on_select: function() { #{redraw(:asset, [ key, t(value).downcase ], url_for(:action => :redraw))} } }]
     end
   end
 
   #----------------------------------------------------------------------------
   def sort_by_duration
     Activity::DURATION.map do |key, value|
-      %Q[{ name: "#{value}", on_select: function() { #{redraw(:duration, [ key, value.downcase ], url_for(:action => :redraw))} } }]
+      %Q[{ name: "#{t(value)}", on_select: function() { #{redraw(:duration, [ key, t(value).downcase ], url_for(:action => :redraw))} } }]
     end
   end
 
@@ -60,3 +60,4 @@ module HomeHelper
       :default => "#{user} #{action} #{type} #{subject}")
   end
 end
+
