299d3429789fb7ed9f1653f993b555d93e0b0f01
Seb Jacobs <seb.jacobs@unboxedconsulting.com>
2011-05-12 12:08:37 +0100

    fixed user_mailer typo

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -24,7 +24,7 @@ class UserMailer < ActionMailer::Base
   
   def assigned_to_resource_email_notification(resource)
     subject "You have been assigned #{@resource_name} #{resource.class.name} in CRM"
-    from "CRM donotreply@crm.unboxedconsulting.com"
+    from "CRM <crm@unboxedconsulting.com>"
     reply_to "#{resource.user.email}"
     recipients "#{resource.assignee.email}"
     body :resource_url => self.send("#{resource.class.name.downcase}_url".to_sym, resource.id, :protocol => 'https', :host => "crm.unboxedconsulting.com"),
