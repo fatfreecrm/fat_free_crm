000cff7ddd0b006710d1372961dc951e3563896c
Nathan Broadbent <nathan.f77@gmail.com>
2011-07-21 17:18:52 +0800

    Reverted contact name validations.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -89,9 +89,8 @@ class Contact < ActiveRecord::Base
   exportable
   sortable :by => [ "first_name ASC",  "last_name ASC", "created_at DESC", "updated_at DESC" ], :default => "created_at DESC"
 
-  # Validate that either first_name OR last_name is present.
-  validates_presence_of :first_name, :message => :missing_first_name, :unless => Proc.new {|c| c.last_name.present? }
-  validates_presence_of :last_name, :message => :missing_last_name, :unless => Proc.new {|c| c.first_name.present? }
+  validates_presence_of :first_name, :message => :missing_first_name
+  validates_presence_of :last_name, :message => :missing_last_name
   validate :users_for_shared_access
 
   # Default values provided through class methods.
