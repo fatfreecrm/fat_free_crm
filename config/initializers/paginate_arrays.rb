70a9df5aab024ca8297a50092a44c5a6b9552aef
Nathan Broadbent <nathan.f77@gmail.com>
2011-08-31 18:01:57 +0800

    Require 'will_paginate/array' to .. well, paginate arrays.

---
+++ config/initializers/paginate_arrays.rb
@@ -0,0 +1,7 @@
+# From https://github.com/mislav/will_paginate/wiki/Backwards-incompatibility
+#
+#    The Array#paginate method still exists, too, but is not loaded by default.
+#    If you need to paginate static arrays, first require it in your code:
+#
+require 'will_paginate/array'
+
