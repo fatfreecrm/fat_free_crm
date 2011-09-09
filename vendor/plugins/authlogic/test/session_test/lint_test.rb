1965c454ac7b8940d9771ba4359d3302697acb62
Nathan Broadbent <nathan.f77@gmail.com>
2011-09-09 11:22:49 +0800

    Removed vendored authlogic, use authlogic from rubygems.org

--- vendor/plugins/authlogic/test/session_test/lint_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper.rb'
-
-class LintTest < ActiveModel::TestCase
-  include ActiveModel::Lint::Tests
- 
-  def setup
-    @model = UserSession.new
-  end
-end
