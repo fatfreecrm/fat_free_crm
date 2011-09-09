09c90f5bc75c22183231bd6bf4972d4d293b5fb3
Nathan Broadbent <nathan.f77@gmail.com>
2011-09-09 16:01:59 +0800

    Removed authlogic_api since it breaks the latest authlogic

--- vendor/plugins/authlogic_api/init.rb
+++
@@ -1,2 +0,0 @@
-# Include hook code here
-require 'authlogic_api'
--- vendor/plugins/authlogic_api/lib/authlogic_api.rb
+++
@@ -1,5 +0,0 @@
-require "authlogic_api/acts_as_authentic"
-require "authlogic_api/session"
-
-ActiveRecord::Base.send(:include, AuthlogicApi::ActsAsAuthentic)
-Authlogic::Session::Base.send(:include, AuthlogicApi::Session)
