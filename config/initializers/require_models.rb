e18e9d951dd701465ed775ecb541d814cdcd29db
Nathan Broadbent <nathan.f77@gmail.com>
2011-11-23 16:01:20 +0800

    Fixed is_paranoid super from singleton method bug

--- config/initializers/require_models.rb
+++
@@ -1,9 +0,0 @@
-#
-# We need to require our models here, since they are organized in subdirectories.
-# If we don't do this, is_paranoid raises the following error when running tests:
-#
-#     super from singleton method that is defined to multiple classes is not supported;
-#     this will be fixed in 1.9.3 or later
-#
-Dir[Rails.root.join("app/models/**/*.rb")].each {|f| require f }
-
