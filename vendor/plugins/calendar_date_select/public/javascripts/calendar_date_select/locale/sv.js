b4c916947fa6996b53bd8c3e0530899dbd6b9ec1
Ben Tillman <bstillman@crossroadsint.org>
2011-11-25 15:44:12 +0800

    Removed calendar_date_Select because it was not rails 3.1 friendly.

--- vendor/plugins/calendar_date_select/public/javascripts/calendar_date_select/locale/sv.js
+++
@@ -1,9 +0,0 @@
-Date.weekdays = $w('Må Ti On To Fr Lö Sö');
-Date.months = $w('Januari Februari Mars April Maj Juni Juli Augusti September Oktober November December');
-Date.first_day_of_week = 1;
-_translations = {
-  "OK": "OK",
-  "Now": "Nu",
-  "Today": "Idag",
-  "Clear": "Avbryt"
-}
