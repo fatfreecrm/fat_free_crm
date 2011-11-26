0e085873cf3221ce2a71756854ff7015ef3cece3
Nathan Broadbent <nathan.f77@gmail.com>
2011-11-26 11:37:22 +0800

    Renamed mail_host to host

--- config/initializers/fat_free_crm_action_mailer.rb
+++ config/initializers/fat_free_crm_action_mailer.rb
@@ -1,3 +1,3 @@
-if Setting.table_exists? and !Setting.mail_host.nil?
-  Rails.application.config.action_mailer.default_url_options[:host] = Setting.mail_host
+if Setting.table_exists? and Setting.host.present?
+  (FatFreeCRM::Application.config.action_mailer.default_url_options ||= {})[:host] = Setting.host
 end
