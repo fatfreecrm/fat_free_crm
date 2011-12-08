1c219d2c50141a5062846991e2a554d9aef4e172
Ben Tillman <bstillman@crossroadsint.org>
2011-12-08 15:42:48 +0800

    Only try to YAML load non nil values

--- app/helpers/fields_helper.rb
+++ app/helpers/fields_helper.rb
@@ -12,7 +12,7 @@ module FieldsHelper
       value && value.strftime(I18n.t("time.formats.short"))
     when 'check_boxes'
       # Comma separated, 2 per line.
-      YAML.load(value).in_groups_of(2, false).map {|g| g.join(', ')}.join(tag(:br))
+      YAML.load(value).in_groups_of(2, false).map {|g| g.join(', ')}.join(tag(:br)) if value.present?
     else
       value.to_s
     end
