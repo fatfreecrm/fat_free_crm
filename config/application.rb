a7a0ee0ed6d6d78782d6effdf698fb52de5f1308
Nathan Broadbent <nathan.f77@gmail.com>
2011-12-08 12:11:49 +0800

    Fixed deprecation warning for accessing Rails paths

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ end
 # Override engine views so that plugin views have higher priority.
 Rails::Engine.initializers.detect{|i| i.name == :add_view_paths }.
   instance_variable_set("@block", Proc.new {
-    views = paths.app.views.to_a
+    views = paths["app/views"].to_a
     unless views.empty?
       ActiveSupport.on_load(:action_controller){ append_view_path(views) }
       ActiveSupport.on_load(:action_mailer){ append_view_path(views) }
