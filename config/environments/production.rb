ef2dc2d3a0457ae9d44bb3199db191608b53fe6a
Nathan Broadbent <nathan.f77@gmail.com>
2011-12-09 13:50:39 +0800

    Reset perform_caching to true for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ FatFreeCRM::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = !ENV['HEROKU'] # Don't cache assets for heroku.
+  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
