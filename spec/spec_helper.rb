0aa33c08b8dd729d128a6eb3283c3d31c5da9219
Nathan Broadbent <nathan.f77@gmail.com>
2011-12-10 02:50:36 +0800

    Fixed 'rake spec' task to always use test environment.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ if ENV["COVERAGE"]
 end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] = 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
