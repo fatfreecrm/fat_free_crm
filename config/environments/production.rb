0154937c590a6a5ad364a0bfd3e16e0d16ef8ef4
Nathan Broadbent <nathan.f77@gmail.com>
2011-12-09 13:06:11 +0800

    Compile print.sass -> print.css, added print.css to config.assets.precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ FatFreeCRM::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( print.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
