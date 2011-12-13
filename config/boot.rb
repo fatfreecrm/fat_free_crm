e41870836fec4f514940e75a85e430bb7216cfa1
Nathan Broadbent <nathan.f77@gmail.com>
2011-12-13 23:03:24 +0800

    Updated config/boot.rb to Rails 3.1 template, since ours was outdated. Critical for Travis build, since ENV['BUNDLE_GEMFILE'] needs precedence. Also added extra files to .gitignore, and install gems without heroku group when testing

--- config/boot.rb
+++ config/boot.rb
@@ -6,14 +6,6 @@ if RUBY_VERSION.to_f >= 1.9
 end
 
 # Set up gems listed in the Gemfile.
-gemfile = File.expand_path('../../Gemfile', __FILE__)
-begin
-  ENV['BUNDLE_GEMFILE'] = gemfile
-  require 'bundler'
-  Bundler.setup
-rescue Bundler::GemNotFound => e
-  STDERR.puts e.message
-  STDERR.puts "Try running `bundle install`."
-  exit!
-end if File.exist?(gemfile)
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
