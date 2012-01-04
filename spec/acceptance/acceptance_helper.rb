313303a29d865510aaabbd8a56b3cfc0b6708b10
Steve Kenworthy <steveyken@gmail.com>
2012-01-04 12:41:08 +0800

    ChromeDriver is now optional. Firefox works out of the box. (http://code.google.com/p/selenium/wiki/ChromeDriver)

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -38,8 +38,10 @@ RSpec.configuration.before(:each, :type => :acceptance) do
 end
 
 # Chrome browser
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+if ENV['BROWSER'] == 'chrome'
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  end
 end
 
 # Default timeout for extended for AJAX based application
