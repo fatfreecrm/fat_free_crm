887f96085a04cdd330f1602b9324d8e624fba2c7
Steve Kenworthy <steveyken@gmail.com>
2012-01-06 17:30:38 +0800

    Added ajax search test. Added login helper that only logs in if user is not already logged in.

--- spec/acceptance/accounts_spec.rb
+++ spec/acceptance/accounts_spec.rb
@@ -7,7 +7,7 @@ feature 'Accounts', %q{
 } do
 
   before(:each) do
-    do_login
+   do_login_if_not_already
   end
 
   scenario 'should view a list of accounts' do
@@ -26,6 +26,54 @@ feature 'Accounts', %q{
     fill_in 'account_name', :with => 'My new account'
     click_button 'Create Account'
     page.should have_content('My new account')
+  end
+
+  scenario 'should ajax search for an account', :js => true do
+    2.times { |i| Factory(:account, :name => "Account #{i}") }
+    visit accounts_path
+    find('#accounts').should have_content("Account 0")
+    find('#accounts').should have_content("Account 1")
+    fill_in 'query', :with => "Account 0"
+    find('#accounts').should have_content("Account 0")
+    find('#accounts').has_selector?('li', :count => 1)
+    fill_in 'query', :with => "Account"
+    find('#accounts').should have_content("Account 0")
+    find('#accounts').should have_content("Account 1")
+    find('#accounts').has_selector?('li', :count => 2)
+    fill_in 'query', :with => "Contact"
+    find('#accounts').has_selector?('li', :count => 0)
+  end
+
+  scenario 'should ajax search for an account', :js => true do
+    2.times { |i| Factory(:account, :name => "Account #{i}") }
+    visit accounts_path
+    find('#accounts').should have_content("Account 0")
+    find('#accounts').should have_content("Account 1")
+    fill_in 'query', :with => "Account 0"
+    find('#accounts').should have_content("Account 0")
+    find('#accounts').has_selector?('li', :count => 1)
+    fill_in 'query', :with => "Account"
+    find('#accounts').should have_content("Account 0")
+    find('#accounts').should have_content("Account 1")
+    find('#accounts').has_selector?('li', :count => 2)
+    fill_in 'query', :with => "Contact"
+    find('#accounts').has_selector?('li', :count => 0)
+  end
+
+  scenario 'should ajax search for an account', :js => true do
+    2.times { |i| Factory(:account, :name => "Account #{i}") }
+    visit accounts_path
+    find('#accounts').should have_content("Account 0")
+    find('#accounts').should have_content("Account 1")
+    fill_in 'query', :with => "Account 0"
+    find('#accounts').should have_content("Account 0")
+    find('#accounts').has_selector?('li', :count => 1)
+    fill_in 'query', :with => "Account"
+    find('#accounts').should have_content("Account 0")
+    find('#accounts').should have_content("Account 1")
+    find('#accounts').has_selector?('li', :count => 2)
+    fill_in 'query', :with => "Contact"
+    find('#accounts').has_selector?('li', :count => 0)
   end
 
 end
--- spec/acceptance/support/helpers.rb
+++ spec/acceptance/support/helpers.rb
@@ -1,12 +1,17 @@
 module HelperMethods
   # Put helper methods you need to be available in all acceptance specs here.
 
-  def do_login(args = {})
-    user = Factory(:user, args)
+  def do_login(options = {})
+    @user = Factory(:user, options)
     visit '/login'
-    fill_in "authentication_username", :with => user.username
-    fill_in "authentication_password", :with => user.password
+    fill_in "authentication_username", :with => @user.username
+    fill_in "authentication_password", :with => @user.password
     click_button "Login"
+  end
+
+  # if we're already logged in, don't bother doing it again
+  def do_login_if_not_already(options = {})
+    do_login(options) unless @user.present?
   end
 
 end
