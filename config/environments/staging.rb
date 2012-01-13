d7624813c2198b116054244494f5fd10b97c2839
Nathan Broadbent <nathan.f77@gmail.com>
2012-01-13 16:38:09 +0800

    Added staging environment, refactored all example database.yml files [ci skip]

---
+++ config/environments/staging.rb
@@ -0,0 +1,13 @@
+require Rails.root.join('config', 'environments','production')
+
+FatFreeCRM::Application.configure do
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = false
+
+  # See everything in the log (default is :info)
+  config.log_level = :debug
+
+  # Full error reports
+  config.consider_all_requests_local = true
+end
