09b09aad227bebcfea97c4c8d9e71b4ceee8c24f
Nathan Broadbent <nathan.f77@gmail.com>
2012-02-17 01:42:16 +0800

    Correction destroy action for admin tags and field_groups controllers.

--- app/models/polymorphic/tag.rb
+++ app/models/polymorphic/tag.rb
@@ -1,10 +1,8 @@
 class Tag < ActsAsTaggableOn::Tag
+  before_destroy :no_associated_field_groups
 
-  before_destroy :check_if_associated_field_groups
-
-  private
   # Don't allow a tag to be deleted if it is associated with a Field Group
-  def check_if_associated_field_groups
+  def no_associated_field_groups
     FieldGroup.find_all_by_tag_id(self).none?
   end
 end
