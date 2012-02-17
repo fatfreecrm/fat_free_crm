85ad64f032cd0e810c7e111350d544925b67c47a
Nathan Broadbent <nathan.f77@gmail.com>
2012-02-17 13:11:55 +0800

    Show a count of tags per model in admin interface. Improved conditional 'Delete' link logic

--- app/models/polymorphic/tag.rb
+++ app/models/polymorphic/tag.rb
@@ -5,4 +5,10 @@ class Tag < ActsAsTaggableOn::Tag
   def no_associated_field_groups
     FieldGroup.find_all_by_tag_id(self).none?
   end
+  
+  # Returns a count of taggings per model klass
+  # e.g. {"Contact" => 3, "Account" => 1}
+  def model_tagging_counts
+    Tagging.where(:tag_id => id).count(:group => :taggable_type)
+  end
 end
