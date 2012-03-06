2a7cef210abb933ac65595b66f33d3c6e503300b
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-06 15:52:14 +0800

    Added secret token for Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,13 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+
+# PLEASE NOTE: This secret token must be changed in your fork of Fat Free CRM.
+# This problem is mitigated when running Fat Free CRM as a Rails Engine.
+
+if defined?(FatFreeCRM::Application)
+  FatFreeCRM::Application.config.secret_token = '51aa366864a80316a85cff0d3762347f4ae3d029d548bef034d56e82b1a2ffac5353ee6719d9b64e4354e2a0b1a901679f46a851c360a2ea377188e4b196b6b6'
+end
