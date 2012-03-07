0c41b1b813445194a2b68a7367646926c4295f82
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-07 11:33:30 +0800

    Only load environments if FatFreeCRM::Application is defined. (Fixes problem with Rails Engines)

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,13 +1,14 @@
-require Rails.root.join('config', 'environments', 'production')
+if defined?(FatFreeCRM::Application)
+  require Rails.root.join('config', 'environments', 'production')
+  FatFreeCRM::Application.configure do
+    # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+    # the I18n.default_locale when a translation can not be found)
+    config.i18n.fallbacks = false
 
-FatFreeCRM::Application.configure do
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = false
+    # See everything in the log (default is :info)
+    config.log_level = :debug
 
-  # See everything in the log (default is :info)
-  config.log_level = :debug
-
-  # Full error reports
-  config.consider_all_requests_local = true
+    # Full error reports
+    config.consider_all_requests_local = true
+  end
 end
