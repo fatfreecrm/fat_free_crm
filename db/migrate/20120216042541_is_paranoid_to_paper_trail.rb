0c77981417cc4c307e5d894bb7073a33a2889b2d
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-08 13:05:01 +0800

    Fixed papertrail migration

--- db/migrate/20120216042541_is_paranoid_to_paper_trail.rb
+++ db/migrate/20120216042541_is_paranoid_to_paper_trail.rb
@@ -3,7 +3,7 @@ class IsParanoidToPaperTrail < ActiveRecord::Migration
     [Account, Campaign, Contact, Lead, Opportunity, Task].each do |klass|
       klass.where('deleted_at IS NOT NULL').each do |object|
         object.destroy          # Really destroy the object
-        Activities.last.destroy # Remove the destroy activity because we should already have one indicating this object is destroyed
+        Activity.last.destroy   # Remove the destroy activity because we should already have one indicating this object is destroyed
       end
     end
   end
