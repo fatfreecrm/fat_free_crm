6aa3a6760b9ec0190255f6e6b827426e154da6d1
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-09 16:33:48 +0800

    Don't initialize Rails environment for asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,9 @@ module FatFreeCRM
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Don't initialize Rails environment
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
