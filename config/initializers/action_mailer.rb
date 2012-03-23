2a31c9b5c34741800dc7ad3c839e979450038dcc
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-23 18:07:22 +0800

    Set actionmailer host in test environment

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -6,9 +6,9 @@ unless Rails.env.test?
     Rails.application.config.action_mailer.delivery_method = :smtp
     Rails.application.config.action_mailer.smtp_settings = smtp_settings
   end
+end
 
-  # Set default host for outgoing emails
-  if Setting.host.present?
-    (Rails.application.config.action_mailer.default_url_options ||= {})[:host] = Setting.host
-  end
+# Set default host for outgoing emails
+if Setting.host.present?
+  (Rails.application.config.action_mailer.default_url_options ||= {})[:host] = Setting.host
 end
