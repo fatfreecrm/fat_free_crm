2875d4e2f6144c26fccf2cbcb55f34adbf904217
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-23 15:18:55 +0800

    Don't set ActionMailer settings in test environment

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,12 +1,14 @@
-# Configure ActionMailer
+# Configure ActionMailer unless running tests
 
-# Set SMTP settings if present.
-if smtp_settings = Setting.smtp
-  Rails.application.config.action_mailer.delivery_method = :smtp
-  Rails.application.config.action_mailer.smtp_settings = smtp_settings
-end
+unless Rails.env.test?
+  # Set SMTP settings if present.
+  if smtp_settings = Setting.smtp
+    Rails.application.config.action_mailer.delivery_method = :smtp
+    Rails.application.config.action_mailer.smtp_settings = smtp_settings
+  end
 
-# Set default host for outgoing emails
-if Setting.host.present?
-  (Rails.application.config.action_mailer.default_url_options ||= {})[:host] = Setting.host
+  # Set default host for outgoing emails
+  if Setting.host.present?
+    (Rails.application.config.action_mailer.default_url_options ||= {})[:host] = Setting.host
+  end
 end
