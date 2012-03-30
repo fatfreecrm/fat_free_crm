dc948ab4121992a1d879aa139bb1c31fab06f83d
Nathan Broadbent <nathan.f77@gmail.com>
2012-03-30 12:22:30 +0800

    Only set PaperTrail user if user can be found

--- lib/fat_free_crm/mail_processor/base.rb
+++ lib/fat_free_crm/mail_processor/base.rb
@@ -163,9 +163,10 @@ module FatFreeCRM
 
       #------------------------------------------------------------------------------
       def find_sender(email_address)
-        @sender = User.first(:conditions => [ "(lower(email) = ? OR lower(alt_email) = ?) AND suspended_at IS NULL", email_address.downcase, email_address.downcase ])
-        # Set the PaperTrail user for versions
-        PaperTrail.whodunnit = @sender.id.to_s
+        if @sender = User.first(:conditions => [ "(lower(email) = ? OR lower(alt_email) = ?) AND suspended_at IS NULL", email_address.downcase, email_address.downcase ])
+          # Set the PaperTrail user for versions (if user is found)
+          PaperTrail.whodunnit = @sender.id.to_s
+        end
       end
 
       #--------------------------------------------------------------------------------------
