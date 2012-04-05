0eb5e6f2f28be0da1f0ffe2766dd562861f2b9eb
Nathan Broadbent <nathan.f77@gmail.com>
2012-04-05 22:10:39 +0800

    Replaced for loops with #each blocks in migrations

--- db/migrate/20120405080742_change_further_subscribed_users_to_set.rb
+++ db/migrate/20120405080742_change_further_subscribed_users_to_set.rb
@@ -7,14 +7,14 @@ class ChangeFurtherSubscribedUsersToSet < ActiveRecord::Migration
         FROM #{table}
         WHERE subscribed_users IS NOT NULL
       }
-      
+
       puts "#{table}: Converting #{entities.size} subscribed_users arrays into sets..." unless entities.empty?
-      
+
       # Run as one atomic action.
       ActiveRecord::Base.transaction do
-        for entity in entities
+        entities.each do |entity|
           subscribed_users_set = Set.new(YAML.load(entity["subscribed_users"]))
-          
+
           connection.execute %Q{
             UPDATE #{table}
             SET subscribed_users = '#{subscribed_users_set.to_yaml}'
