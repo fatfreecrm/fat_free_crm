92f58e2e540a7b66b85fd62e640cd406383e814f
Ben Tillman <ben.tillman@gmail.com>
2012-04-13 12:35:38 +0800

    Group permissions performance tweaks

--- app/models/users/ability.rb
+++ app/models/users/ability.rb
@@ -11,8 +11,18 @@ class Ability
       can :manage, entities, :access => 'Public'
       can :manage, entities + [Task], :user_id => user.id
 
+      # Group or User permissions
+      t = Permission.arel_table
+      scope = t[:user_id].eq(user.id)
+
+      if (group_ids = user.group_ids).any?
+        scope = scope.or(t[:group_id].eq(group_ids))
+      end
+
       entities.each do |klass|
-        can :manage, klass, :id => Permission.where('asset_type = ? AND (user_id = ? OR group_id = ?)', klass.name, user.id, user.group_ids).value_of(:asset_id)
+        if (asset_ids = Permission.where(scope.and(t[:asset_type].eq(klass.name))).value_of(:asset_id)).any?
+          can :manage, klass, :id => asset_ids
+        end
       end
     end
   end
