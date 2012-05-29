0d9e21b3f7b522eb8bde88a50cc539aeadc10d6c
Steve Kenworthy <steveyken@gmail.com>
2012-05-29 17:16:07 +0800

    Update schema db with group permissions.

--- db/schema.rb
+++ db/schema.rb
@@ -227,6 +227,21 @@ ActiveRecord::Schema.define(:version => 20120528102124) do
   add_index "fields", ["field_group_id"], :name => "index_fields_on_field_group_id"
   add_index "fields", ["name"], :name => "index_fields_on_name"
 
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "groups_users", :id => false, :force => true do |t|
+    t.integer "group_id"
+    t.integer "user_id"
+  end
+
+  add_index "groups_users", ["group_id", "user_id"], :name => "index_groups_users_on_group_id_and_user_id"
+  add_index "groups_users", ["group_id"], :name => "index_groups_users_on_group_id"
+  add_index "groups_users", ["user_id"], :name => "index_groups_users_on_user_id"
+
   create_table "leads", :force => true do |t|
     t.integer  "user_id"
     t.integer  "campaign_id"
@@ -295,9 +310,11 @@ ActiveRecord::Schema.define(:version => 20120528102124) do
     t.string   "asset_type"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "group_id"
   end
 
   add_index "permissions", ["asset_id", "asset_type"], :name => "index_permissions_on_asset_id_and_asset_type"
+  add_index "permissions", ["group_id"], :name => "index_permissions_on_group_id"
   add_index "permissions", ["user_id"], :name => "index_permissions_on_user_id"
 
   create_table "preferences", :force => true do |t|
