e080ac8b5fd5aaf8415435beef9090250e646f8e
Chris Carter <chris.carter@unboxedconsulting.com>
2012-06-06 14:36:48 +0100

    A comment can be added to a contact during creation.

--- acceptance/contacts_spec.rb
+++ acceptance/contacts_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path("../acceptance_helper.rb", __FILE__)
 
-feature 'Accounts', %q{
+feature 'Contacts', %q{
   In order to increase customer satisfaction
   As a user
   I want to manage contacts
@@ -28,12 +28,29 @@ feature 'Accounts', %q{
     fill_in 'contact_last_name', :with => 'McTest'
     fill_in 'contact_email', :with => "testy.mctest@example.com"
     fill_in 'contact_phone', :with => '+44 1234 567890'
+    click_link 'Comment'
+    fill_in 'comment_body', :with => 'This is a very important person.'
     click_button 'Create Contact'
     page.should have_content('Testy McTest')
+
+    click_link 'Testy McTest'
+    page.should have_content('This is a very important person.')
 
     click_link "Dashboard"
     page.should have_content('Bill Murray created contact Testy McTest')
     page.should have_content('Bill Murray created address on Testy McTest')
+    page.should have_content('Bill Murray created comment on Testy McTest')
+  end
+
+  scenario "remembers the comment field when the creation was unsuccessful", :js => true do
+    visit contacts_page
+    click_link 'Create Contact'
+
+    click_link 'Comment'
+    fill_in 'comment_body', :with => 'This is a very important person.'
+    click_button 'Create Contact'
+
+    page.should have_field("comment_body", :with => 'This is a very important person.')
   end
 
   scenario 'should view and edit a contact', :js => true do
--- app/models/entities/contact.rb
+++ app/models/entities/contact.rb
@@ -83,6 +83,7 @@ class Contact < ActiveRecord::Base
 
   uses_user_permissions
   acts_as_commentable
+  uses_comment_extensions
   acts_as_taggable_on :tags
   has_paper_trail :ignore => [ :subscribed_users ]
   has_fields
