55c1936f4693c309530a3b111d2aa1b13e994102
Chris Carter <chris.carter@unboxedconsulting.com>
2012-06-08 16:55:44 +0100

    fixed dashboard index spec

--- spec/views/home/index.haml_spec.rb
+++ spec/views/home/index.haml_spec.rb
@@ -9,14 +9,18 @@ describe "/home/index" do
 
   it "should render list of activities if it's not empty" do
     assign(:activities, [ FactoryGirl.create(:version, :event => "update", :item => FactoryGirl.create(:account)) ])
-
+    assign(:my_tasks, [])
+    assign(:my_opportunities, [])
+    assign(:my_accounts, [])
     render
     view.should render_template(:partial => "_activity")
   end
 
   it "should render a message if there're no activities" do
     assign(:activities, [])
-
+    assign(:my_tasks, [])
+    assign(:my_opportunities, [])
+    assign(:my_accounts, [])
     render
     view.should_not render_template(:partial => "_activity")
 
