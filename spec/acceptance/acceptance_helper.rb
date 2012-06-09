ff8b23ed8677b9c8aac0fe1675da38a70e76c65f
Seb Jacobs <me@sebjacobs.com>
2012-06-09 16:14:29 +0100

    moved acceptance spec from /acceptance to spec/acceptance

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -1,4 +1,4 @@
-require File.expand_path("../../spec/spec_helper.rb", __FILE__)
+require 'spec_helper'
 
 # Put your acceptance spec helpers inside /spec/acceptance/support
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
