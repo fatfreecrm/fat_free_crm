0aa5553292f531e02b15447a5d47351dc415ddac
Chris Carter <chris.carter@unboxedconsulting.com>
2012-06-11 12:58:54 +0100

    added unassigned scope to opportunity model

--- app/models/entities/opportunity.rb
+++ app/models/entities/opportunity.rb
@@ -59,6 +59,7 @@ class Opportunity < ActiveRecord::Base
   scope :lost,     where("opportunities.stage = 'lost'")
   scope :not_lost, where("opportunities.stage <> 'lost'")
   scope :pipeline, where("opportunities.stage IS NULL OR (opportunities.stage != 'won' AND opportunities.stage != 'lost')")
+  scope :unassigned,  where("opportunities.assigned_to IS NULL")
 
   # Search by name OR id
   scope :text_search, lambda { |query|
