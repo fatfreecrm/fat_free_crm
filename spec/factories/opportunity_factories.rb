b464ebb015d190545b3e65ef90cc3a4c9cff02c0
Seb Jacobs <me@sebjacobs.com>
2012-06-28 10:31:56 +0100

    made user spec more stable, added an open_opportunity factory

--- spec/factories/opportunity_factories.rb
+++ spec/factories/opportunity_factories.rb
@@ -17,4 +17,8 @@ FactoryGirl.define do
     updated_at          { FactoryGirl.generate(:time) }
     created_at          { FactoryGirl.generate(:time) }
   end
+
+  factory :open_opportunity, :parent => :opportunity do
+    stage               { %w(prospecting analysis presentation proposal negotiation final_review).sample }
+  end
 end
