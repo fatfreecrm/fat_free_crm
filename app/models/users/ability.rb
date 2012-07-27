75f6b5778eb8af42010c9ee80c9ce2e0e6b81bd1
Steve Kenworthy <steveyken@gmail.com>
2012-07-27 12:59:54 +0800

    Work around for obscure bug where not all entities for which the user had permissions were returned. See https://github.com/ryanb/cancan/issues/213 for more details.

--- app/models/users/ability.rb
+++ app/models/users/ability.rb
@@ -12,21 +12,29 @@ class Ability
       can :manage, entities + [Task], :user_id => user.id
       can :manage, entities + [Task], :assigned_to => user.id
       
-      can :manage, :all if user.admin?
+      #
+      # Due to an obscure bug (see https://github.com/ryanb/cancan/issues/213)
+      # we must switch on user.admin? here to avoid the nil constraints which
+      # activate the issue referred to above.
+      #
+      if user.admin?
+        can :manage, :all
+      else
+        # Group or User permissions
+        t = Permission.arel_table
+        scope = t[:user_id].eq(user.id)
 
-      # Group or User permissions
-      t = Permission.arel_table
-      scope = t[:user_id].eq(user.id)
-
-      if (group_ids = user.group_ids).any?
-        scope = scope.or(t[:group_id].eq_any(group_ids))
-      end
-
-      entities.each do |klass|
-        if (asset_ids = Permission.where(scope.and(t[:asset_type].eq(klass.name))).value_of(:asset_id)).any?
-          can :manage, klass, :id => asset_ids
+        if (group_ids = user.group_ids).any?
+          scope = scope.or(t[:group_id].eq_any(group_ids))
         end
-      end
+
+        entities.each do |klass|
+          if (asset_ids = Permission.where(scope.and(t[:asset_type].eq(klass.name))).value_of(:asset_id)).any?
+            can :manage, klass, :id => asset_ids
+          end
+        end
+      end # if user.admin?
+      
     end
   end
 end
