80210b37ce8cecebf33964a35b2bfd42feb6712c
Adrian Klingel <Adrian.Klingel@illumaware.com>
2012-09-03 15:28:55 -0400

    removed autocomplete call from leads since it's a mysterious thing that doesn't work

--- app/controllers/entities/leads_controller.rb
+++ app/controllers/entities/leads_controller.rb
@@ -17,7 +17,7 @@
 
 class LeadsController < EntitiesController
   before_filter :get_data_for_sidebar, :only => :index
-  autocomplete :account, :name, :full => true
+  #autocomplete :account, :name, :full => true
 
   # GET /leads
   #----------------------------------------------------------------------------
