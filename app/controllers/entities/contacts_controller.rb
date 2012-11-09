1f231799974257b0f00fdb09177a2e345d90904e
Reuben Salagaras <reuben.salagaras@gmail.com>
2012-11-09 22:56:01 +1030

    remove debug includes

--- app/controllers/entities/contacts_controller.rb
+++ app/controllers/entities/contacts_controller.rb
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #------------------------------------------------------------------------------
-require 'ruby-debug'
 class ContactsController < EntitiesController
   before_filter :get_users, :only => [ :new, :create, :edit, :update ]
   before_filter :get_accounts, :only => [ :new, :create, :edit, :update ]
--- app/controllers/entities/event_instances_controller.rb
+++ app/controllers/entities/event_instances_controller.rb
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #------------------------------------------------------------------------------
-#require 'ruby-debug'
 class EventInstancesController < EntitiesController
   before_filter :get_data_for_sidebar, :only => :index
 
