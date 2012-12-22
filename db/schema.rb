12457f7507755032c68fc052c3b2ab7f5adca78e
Steve Kenworthy <steveyken@gmail.com>
2012-12-22 14:47:51 +0800

    New schema version.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121003063155) do
+ActiveRecord::Schema.define(:version => 20121221033947) do
 
   create_table "account_contacts", :force => true do |t|
     t.integer  "account_id"
