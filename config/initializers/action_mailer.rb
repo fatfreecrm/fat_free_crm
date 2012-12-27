64171e165e3c2eff6ee6c4587f128163cb9c60f3
Steve Kenworthy <steveyken@gmail.com>
2012-12-27 16:49:34 +0800

    Fixed action_mailer routing settings. Issue #219

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,15 +1,17 @@
+#
 # Configure ActionMailer unless running tests
-
+#   ActionMailer is setup in test mode later on
+#
 unless Rails.env.test?
-  # Set SMTP settings if present.
-  smtp_settings = Setting.smtp || {}
-  if smtp_settings["address"].present?
-    Rails.application.config.action_mailer.delivery_method = :smtp
-    Rails.application.config.action_mailer.smtp_settings = smtp_settings
-  end
-end
 
-# Set default host for outgoing emails
-if Setting.host.present?
-  (Rails.application.config.action_mailer.default_url_options ||= {})[:host] = Setting.host
+  smtp_settings = Setting.smtp || {}
+
+  if smtp_settings["address"].present?
+    Rails.application.config.action_mailer.smtp_settings = smtp_settings.symbolize_keys
+  end
+
+  if (host = Setting.host).present?
+    (Rails.application.routes.default_url_options ||= {})[:host] = host.gsub('http://', '')
+  end
+
 end
