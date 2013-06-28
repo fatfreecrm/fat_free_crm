03b0a151c4cde9027aa0c17f93a8c7979bc91b11
Steve Kenworthy <steveyken@gmail.com>
2013-06-28 12:58:35 +0800

    Collective wisdom suggests moving YAML parser selection to config/application after bundler setup.

--- config/boot.rb
+++ config/boot.rb
@@ -3,9 +3,6 @@
 # Fat Free CRM is freely distributable under the terms of MIT license.
 # See MIT-LICENSE file or http://www.opensource.org/licenses/mit-license.php
 #------------------------------------------------------------------------------
-require 'rubygems'
-require 'yaml'
-YAML::ENGINE.yamler = 'syck'
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
