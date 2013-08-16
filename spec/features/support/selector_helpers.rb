7d9d9de1eaeb9769496bc94e82b31d580eff5efc
Zlatko Zahariev <zlatko.zahariev@insurance-st.com>
2013-08-16 11:07:32 +0300

    Address Issues #242 and #245. Fix cohsen_select helper to be able to work on multiple select boxes with chosen

--- spec/features/support/selector_helpers.rb
+++ spec/features/support/selector_helpers.rb
@@ -5,7 +5,7 @@
 #------------------------------------------------------------------------------
 module SelectorHelpers
   def chosen_select(item_text, options)
-    field_id = find_field(options[:from])[:id]
+    field_id = find_field(options[:from], :visible => false)[:id]
     option_value = page.evaluate_script("jQuery(\"##{field_id} option:contains('#{item_text}')\").val()")#page.evaluate_script("$(\"##{field_id} option:contains('#{item_text}')\").val()")
     page.execute_script("jQuery('##{field_id}').val('#{option_value}')")
   end
