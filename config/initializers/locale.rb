9e869349aaafe1176c207e74e5ffac289522b06c
Steve Kenworthy <steveyken@gmail.com>
2013-12-05 14:27:13 +0800

    Enforce available locales in latest version of I18n.

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -3,6 +3,12 @@
 # Fat Free CRM is freely distributable under the terms of MIT license.
 # See MIT-LICENSE file or http://www.opensource.org/licenses/mit-license.php
 #------------------------------------------------------------------------------
-# Set default locale from Settings
 
-I18n.default_locale = Setting.locale
+# Set default locale from Settings
+# defer setting the locale until all I18n locales have been initialized
+#------------------------------------------------------------------------------
+I18n.config.enforce_available_locales = true
+
+FatFreeCRM.application.config.after_initialize do
+  I18n.default_locale = Setting.locale
+end
