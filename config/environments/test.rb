b14dbe54a29ecffb3dad45043036e8f6072281f9
Matthew Lehner <matthewlehner@gmail.com>
2013-12-07 18:47:31 +0800

    rails 4 dev defaults.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,6 +6,7 @@
 if defined?(FatFreeCRM::Application)
   FatFreeCRM::Application.configure do
     # Settings specified here will take precedence over those in config/application.rb
+    config.eager_load = false
 
     # The test environment is used exclusively to run your application's
     # test suite.  You never need to work with it otherwise.  Remember that
@@ -16,9 +17,6 @@ if defined?(FatFreeCRM::Application)
     # Configure static asset server for tests with Cache-Control for performance
     config.serve_static_assets = true
     config.static_cache_control = "public, max-age=3600"
-
-    # Log error messages when you accidentally call methods on nil
-    config.whiny_nils = true
 
     # Show full error reports and disable caching
     config.consider_all_requests_local       = true
