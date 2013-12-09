0a6f652a24037ae62528bdc3305bb50f5fc5c8a9
Matthew Lehner <matthewlehner@gmail.com>
2013-12-09 09:09:12 +0800

    rails 4 dev defaults.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,40 +6,30 @@
 if defined?(FatFreeCRM::Application)
   FatFreeCRM::Application.configure do
     # Settings specified here will take precedence over those in config/application.rb
+    config.eager_load = false
 
     # In the development environment your application's code is reloaded on
     # every request. This slows down response time but is perfect for development
     # since you don't have to restart the web server when you make code changes.
     config.cache_classes = false
 
-    # Log error messages when you accidentally call methods on nil.
-    config.whiny_nils = true
-
     # Show full error reports and disable caching
     config.consider_all_requests_local       = true
     config.action_controller.perform_caching = false
 
     # Don't care if the mailer can't send
-    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.raise_delivery_errors = false
 
     # Print deprecation notices to the Rails logger
     config.active_support.deprecation = :log
 
     # Only use best-standards-support built into browsers
-    config.action_dispatch.best_standards_support = :builtin
+    # config.action_dispatch.best_standards_support = :builtin
 
-    # Raise exception on mass assignment protection for Active Record models
-    #config.active_record.mass_assignment_sanitizer = :strict
-
-    # Log the query plan for queries taking more than this (works
-    # with SQLite, MySQL, and PostgreSQL)
-    config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-    # Do not compress assets
-    config.assets.compress = false
+    # Raise an error on page load if there are pending migrations
+    config.active_record.migration_error = :page_load
 
     # Expands the lines which load the assets
     config.assets.debug = true
-
   end
 end
