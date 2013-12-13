371b8a66545086f3fe4cde210a7837ab8f468f2b
Steve Kenworthy <steveyken@gmail.com>
2013-12-13 11:09:51 +0800

    Don't enforce locales for the moment. Issue #294

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -7,7 +7,9 @@
 # Set default locale from Settings
 # defer setting the locale until all I18n locales have been initialized
 #------------------------------------------------------------------------------
-I18n.config.enforce_available_locales = true
+
+# Turn off until https://github.com/rails/rails/issues/13164 is fixed
+I18n.config.enforce_available_locales = false
 
 FatFreeCRM.application.config.after_initialize do
   I18n.default_locale = Setting.locale
