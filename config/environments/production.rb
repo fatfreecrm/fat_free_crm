53c40a1b5ff9279240267780c5c8ccdb887bdfcd
Steve Kenworthy <steveyken@gmail.com>
2014-01-16 17:08:59 +0800

    Refactor staging env layout.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ if defined?(FatFreeCRM::Application)
     config.action_controller.perform_caching = true
 
     # Disable Rails's static asset server (Apache or nginx will already do this)
-    config.serve_static_assets = false
+    config.serve_static_assets = true
 
     # Compress JavaScripts and CSS
     config.assets.compress = true
