3d337fa3030a99d180268d49877e852a88a4b61d
Steve Kenworthy <steveyken@gmail.com>
2014-01-25 16:39:21 +0800

    Better position for fallback config (good for engines) [ci skip]

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -10,6 +10,7 @@
 
 # Turn off until https://github.com/rails/rails/issues/13164 is fixed
 I18n.config.enforce_available_locales = false
+Rails.application.config.i18n.fallbacks = true
 
 FatFreeCRM.application.config.after_initialize do
   I18n.default_locale = Setting.locale
