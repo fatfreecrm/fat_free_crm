a0633f428705a1dcec298d960230a18c69115f95
Steve Kenworthy <steveyken@gmail.com>
2014-01-25 12:03:27 +0800

    Revert "Ensure size argument is converted for existing thumbnails too, not just gravatar."
    This reverts commit 76d4351de0bf5cc3819db3ba15fb661dabff9413.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -301,12 +301,14 @@ module ApplicationHelper
   #----------------------------------------------------------------------------
   def avatar_for(model, args = {})
     args = { :class => 'gravatar', :size => :large }.merge(args)
-    args = Avatar.size_from_style!(args) # convert size format :large => '75x75'
+
     if model.respond_to?(:avatar) and model.avatar.present?
       image_tag(model.avatar.image.url(args[:size]), args)
     else
+      args = Avatar.size_from_style!(args) # convert size format :large => '75x75'
       gravatar_image_tag(model.email, args)
     end
+
   end
 
   # Returns default permissions intro.
