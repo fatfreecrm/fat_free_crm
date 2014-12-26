238f4708bdac48db08f9e9a6ea5e1805322734f1
Ben Tillman <ben.tillman@gmail.com>
2014-12-27 00:38:01 +1300

    fixes a couple of AR deprication warnings for connection

--- app/models/fields/custom_field.rb
+++ app/models/fields/custom_field.rb
@@ -125,7 +125,7 @@ class CustomField < Field
   #------------------------------------------------------------------------------
   def add_column
     self.name = generate_column_name if name.blank?
-    connection.add_column(table_name, name, column_type, column_options)
+    klass.connection.add_column(table_name, name, column_type, column_options)
     klass.reset_column_information
     klass.serialize_custom_fields!
   end
@@ -144,7 +144,7 @@ class CustomField < Field
   #------------------------------------------------------------------------------
   def update_column
     if self.errors.empty? && db_transition_safety(as_was) == :safe
-      connection.change_column(table_name, name, column_type, column_options)
+      klass.connection.change_column(table_name, name, column_type, column_options)
       klass.reset_column_information
       klass.serialize_custom_fields!
     end
