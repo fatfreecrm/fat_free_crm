c16883f7007053d2b253771490a3de283b0f53d2
Ben Tillman <ben.tillman@gmail.com>
2014-12-30 15:18:55 +1300

    updates schema dumper initializer

--- lib/fat_free_crm/gem_ext/active_record/schema_dumper.rb
+++ lib/fat_free_crm/gem_ext/active_record/schema_dumper.rb
@@ -11,14 +11,14 @@ require 'active_record'
 unless ENV['INCLUDE_CUSTOM_FIELDS']
   module ActiveRecord
     SchemaDumper.class_eval do
-      def initialize_with_ignored_custom_fields(connection)
+      def initialize_with_ignored_custom_fields(connection, options = {})
         # Override :columns method on this connection, to ignore any custom field columns
         connection.class_eval do
           def columns(*args)
             super.reject { |c| c.name.start_with? "cf_" }
           end
         end
-        initialize_without_ignored_custom_fields(connection)
+        initialize_without_ignored_custom_fields(connection, options)
       end
 
       alias_method_chain :initialize, :ignored_custom_fields
