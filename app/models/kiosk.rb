dc70154dd94e38be7907b70c6308083d0f80b655
Sam Killgallon <s.killgallon@livelinktechnology.net>
2016-03-22 13:42:25 +0000

    Adjust width of new kiosk boxes

--- app/models/kiosk.rb
+++ app/models/kiosk.rb
@@ -2,12 +2,13 @@
 #
 # id                    :integer       Primary key, 70797
 # name                  :string        livelink70797
-# purchase_date         :datetime
+# purchase_date         :date
 # contract_type         :string        bronze, silver, gold, platinum
 # contract_length       :integer       1, 12 (months)
 # password              :string        livelink380
 # cd_password           :string        poster
 # notes                 :text          this kiosk has a custom .....
+# account_id            :integer       account the kiosk is linked too
 
 class Kiosk < ActiveRecord::Base
   validates :name, uniqueness: true
