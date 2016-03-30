e71406bda522f90f056316d7e6a5a24474399fbb
Sam Killgallon <s.killgallon@livelinktechnology.net>
2016-03-30 12:37:28 +0100

    Capitalize instead of downcase the kiosk name

--- app/models/kiosk.rb
+++ app/models/kiosk.rb
@@ -1,17 +1,17 @@
 # Table name: kiosks
 #
 # id                    :integer       Primary key
-# name                  :string        livelink70797
+# name                  :string        Livelink70797, index
 # purchase_date         :date
-# contract_type         :reference     bronze, silver, gold, platinum
+# contract              :reference     bronze silver gold platinum, key
 # contract_length       :integer       1, 12 (months)
 # password              :string        livelink380
 # cd_password           :string        poster
 # notes                 :text          this kiosk has a custom .....
-# account_id            :reference     account the kiosk is linked too
+# account_id            :reference     account the kiosk is linked too, key
 
 class Kiosk < ActiveRecord::Base
-  before_save { self.name.downcase! }
+  before_save { self.name.capitalize! }
   belongs_to :account
   belongs_to :contract
   validates :name, uniqueness: true
