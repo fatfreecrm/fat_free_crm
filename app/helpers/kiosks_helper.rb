7df8e9cfd964723e58d539e5bd97e5e04a8e6df5
Sam Killgallon <s.killgallon@livelinktechnology.net>
2016-03-30 15:51:55 +0100

    Deleted helper and used select_collection instead

--- app/helpers/kiosks_helper.rb
+++ app/helpers/kiosks_helper.rb
@@ -1,11 +1,2 @@
 module KiosksHelper
-
-  def accounts_list
-    Account.all.map { |acc| [acc.name, acc.id] }.sort!
-  end
-
-  def contract_list
-    Contract.all.map { |con| [con.name, con.id] }
-  end
-
 end
