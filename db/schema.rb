bfbf859d1ceac6254244dfa3a1093264b0a0309a
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-05-11 15:10:42 +0930

    #336 Add index for contacts <-> accounts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227123054) do
+ActiveRecord::Schema.define(version: 20160511053730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20150227123054) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "account_contacts", ["account_id", "contact_id"], name: "index_account_contacts_on_account_id_and_contact_id", using: :btree
 
   create_table "account_opportunities", force: :cascade do |t|
     t.integer  "account_id"
