90483230d63995b45848ed7a37fccc749cc87c30
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-12-05 13:50:03 +1030

    #496 Add decorator pattern

--- lib/fat_free_crm/engine.rb
+++ lib/fat_free_crm/engine.rb
@@ -23,5 +23,10 @@ module FatFreeCRM
       end
     end
 
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
