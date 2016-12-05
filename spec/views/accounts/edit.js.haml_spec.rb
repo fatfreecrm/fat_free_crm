07091a37992b2f55f71f8c8a184c96359019b56b
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-12-05 19:42:18 +1030

    Use build_stubbed over create

--- spec/views/accounts/edit.js.haml_spec.rb
+++ spec/views/accounts/edit.js.haml_spec.rb
@@ -10,7 +10,7 @@ describe "/accounts/edit" do
 
   before do
     login_and_assign
-    assign(:account, @account = FactoryGirl.create(:account, user: current_user))
+    assign(:account, @account = FactoryGirl.build_stubbed(:account, user: current_user))
     assign(:users, [current_user])
   end
 
@@ -31,7 +31,7 @@ describe "/accounts/edit" do
 
   it "edit: should hide previously open [Edit Account] for and replace it with account partial" do
     params[:cancel] = nil
-    assign(:previous, previous = FactoryGirl.create(:account, user: current_user))
+    assign(:previous, previous = FactoryGirl.build_stubbed(:account, user: current_user))
 
     render
     expect(rendered).to include("account_#{previous.id}")
