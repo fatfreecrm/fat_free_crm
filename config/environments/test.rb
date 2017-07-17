0b46f5e6a4a378b051c497fa17ed76c3ee757013
Daniel O'Connor <daniel.oconnor@gmail.com>
2017-07-17 12:47:01 +0930

    config.public_file_server.enabled = true

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,7 +17,7 @@ if defined?(FatFreeCRM::Application)
     config.eager_load = false
 
     # Configure static file server for tests with Cache-Control for performance.
-    config.serve_static_files   = true
+    config.public_file_server.enabled = true
     config.static_cache_control = 'public, max-age=3600'
 
     # Show full error reports and disable caching
