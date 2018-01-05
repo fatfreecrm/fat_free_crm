c42cb6522b01a24e9f9b2e9e7dbd69655914c447
Daniel O'Connor <daniel.oconnor@htw.com.au>
2018-01-05 15:07:17 +1030

    Rubocop

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -22,7 +22,7 @@ class Admin::ApplicationController < ApplicationController
   #----------------------------------------------------------------------------
   def require_admin_user
     require_user
-    if current_user && !current_user.admin?
+    unless current_user&.admin?
       flash[:notice] = t(:msg_require_admin)
       redirect_to root_path
     end
