175885e96648f4f6e13215fce01e31b25b8a274f
johnnyshields <johnny.shields@gmail.com>
2018-03-07 20:28:36 +0900

    Upgrade webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,5 @@
 const { environment } = require('@rails/webpacker');
+
 const webpack = require('webpack');
 const erb =  require('./loaders/erb');
 const coffee =  require('./loaders/coffee');
@@ -17,4 +18,5 @@ environment.loaders.get('sass').use.splice(-1, 0, {
 });
 environment.loaders.append('coffee', coffee);
 environment.loaders.append('erb', erb);
+
 module.exports = environment;
