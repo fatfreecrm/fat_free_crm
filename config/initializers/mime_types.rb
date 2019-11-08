6d68fd7998da8c6d93da026ecf500982d17d2fbe
Daniel O'Connor <daniel.oconnor@gmail.com>
2019-11-08 13:14:35 +1030

    Mime type for xls

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,4 +2,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Add new mime types for use in respond_to blocks:
-# Mime::Type.register "text/richtext", :rtf
+Mime::Type.register "application/vnd.msexcel", :xls
