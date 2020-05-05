fe7a5db313c99cf804405729f6da7e373053eeb0
Trey Evans <tevans@justplainawful.lan>
2020-05-05 15:07:55 -0400

    Fix helper constants.

--- spec/helpers/leads_helper_spec.rb
+++ spec/helpers/leads_helper_spec.rb
@@ -7,10 +7,12 @@
 #------------------------------------------------------------------------------
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+module FatFreeCrm
 describe LeadsHelper do
   # Delete this example and add some real ones or delete this file
   it "should be included in the object returned by #helper" do
     included_modules = (class << helper; self; end).send :included_modules
     expect(included_modules).to include(LeadsHelper)
   end
+end
 end
