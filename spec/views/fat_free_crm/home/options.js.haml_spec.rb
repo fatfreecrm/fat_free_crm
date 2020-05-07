d1e7362e2bb7fb18db0fb43b4f603fc3de44a96c
Bill Transue <transue@gmail.com>
2020-05-07 15:08:49 -0400

    Isolate and update Home controller/views

---
+++ spec/views/fat_free_crm/home/options.js.haml_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2008-2013 Michael Dvorkin and contributors.
+#
+# Fat Free CRM is freely distributable under the terms of MIT license.
+# See MIT-LICENSE file or http://www.opensource.org/licenses/mit-license.php
+#------------------------------------------------------------------------------
+require 'spec_helper'
+
+module FatFreeCrm
+  describe "/fat_free_crm/home/options" do
+    before do
+      view.extend ::FatFreeCrm::Engine.routes.url_helpers
+      view.extend FatFreeCrm::ApplicationHelper
+      login
+    end
+
+    it "should render [options] template into :options div and show it" do
+      params[:cancel] = nil
+
+      assign(:asset, "all")
+      assign(:user, "all_users")
+      assign(:action, "all_actions")
+      assign(:duration, "two_days")
+      assign(:all_users, [build_stubbed(:user)])
+
+      render
+
+      expect(rendered).to include("$('#options').html")
+      expect(rendered).to include("$(\\\'#asset\\\').html(\\\'campaign\\\')")
+      expect(rendered).to include("crm.flip_form('options')")
+      expect(rendered).to include("crm.set_title('title', 'Recent Activity Options')")
+    end
+
+    it "should load :options partial with JavaScript code for menus" do
+      params[:cancel] = nil
+      assign(:asset, "all")
+      assign(:action, "all_actions")
+      assign(:user, "all_users")
+      assign(:duration, "two_days")
+      assign(:all_users, [build_stubbed(:user)])
+
+      render
+
+      expect(view).to render_template(partial: "_options")
+    end
+
+    it "should hide options form on Cancel" do
+      params[:cancel] = "true"
+      render
+
+      expect(rendered).not_to include("$('#options').html")
+      expect(rendered).to include("crm.flip_form('options')")
+      expect(rendered).to include("crm.set_title('title', 'Recent Activity')")
+    end
+  end
+end
