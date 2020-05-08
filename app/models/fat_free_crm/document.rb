e5f04e5fd690f119c9abb479645c398b10a8f186
nisanth <nisanth.yaganti@gmail.com>
2020-05-08 08:20:50 -0400

    requiring spec helper

--- app/models/fat_free_crm/document.rb
+++ app/models/fat_free_crm/document.rb
@@ -1,5 +1,14 @@
 module FatFreeCrm
   class Document < ApplicationRecord
     has_one_attached :file
+
+    def uploaded_username
+      user = User.find(uploaded_by_id)
+      user.first_name.to_s + ' ' + user.last_name.to_s
+    end
+
+    def uploaded_by
+      User.find(uploaded_by_id)
+    end
   end
 end
