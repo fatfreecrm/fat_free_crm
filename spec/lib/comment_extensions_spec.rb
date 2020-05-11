9860ba8f73dde38c37754b9a7d3d193e6d1bc591
harshaellanki <h_vardhan@yahoo.com>
2020-05-11 14:51:55 -0400

    fixed comment extensions spec

--- spec/lib/comment_extensions_spec.rb
+++ spec/lib/comment_extensions_spec.rb
@@ -5,36 +5,38 @@
 # Fat Free CRM is freely distributable under the terms of MIT license.
 # See MIT-LICENSE file or http://www.opensource.org/licenses/mit-license.php
 #------------------------------------------------------------------------------
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
 describe FatFreeCrm::CommentExtensions do
   describe "add_comment_by_user" do
     let(:user) { create(:user) }
 
     before do
-      ActiveRecord::Base.connection.create_table(:commentable_entities) do |t|
+      ActiveRecord::Base.connection.create_table(:fat_free_crm_commentable_entities) do |t|
         t.string :subscribed_users
       end
 
-      class CommentableEntity < ActiveRecord::Base
-        serialize :subscribed_users, Set
-        acts_as_commentable
-        uses_comment_extensions
+      module FatFreeCrm
+        class CommentableEntity < ActiveRecord::Base
+          serialize :subscribed_users, Set
+          acts_as_commentable
+          uses_comment_extensions
+        end
       end
     end
 
     after do
-      ActiveRecord::Base.connection.drop_table(:commentable_entities)
+      ActiveRecord::Base.connection.drop_table(:fat_free_crm_commentable_entities)
     end
 
     it "should create a comment for user" do
-      entity = CommentableEntity.create
+      entity = FatFreeCrm::CommentableEntity.create
       entity.add_comment_by_user("I will handle this one", user)
       expect(entity.reload.comments.map(&:comment)).to include("I will handle this one")
     end
 
     it "should not create a comment if body is blank" do
-      entity = CommentableEntity.create
+      entity = FatFreeCrm::CommentableEntity.create
       entity.add_comment_by_user("", user)
       expect(entity.reload.comments).to be_empty
     end
