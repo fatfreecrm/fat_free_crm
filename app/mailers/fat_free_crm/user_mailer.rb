618becf0552c98660f3ce59c693a86ab2d81b022
harshaellanki <h_vardhan@yahoo.com>
2020-05-11 12:53:35 -0400

    demodulize class name in mail body

--- app/mailers/fat_free_crm/user_mailer.rb
+++ app/mailers/fat_free_crm/user_mailer.rb
@@ -10,7 +10,7 @@ module FatFreeCrm
     def assigned_entity_notification(entity, assigner)
       @entity_url = url_for(entity)
       @entity_name = entity.name
-      @entity_type = entity.class.name
+      @entity_type = entity.class.name.demodulize
       @assigner_name = assigner.name
       mail subject: "Fat Free CRM: You have been assigned #{@entity_name} #{@entity_type}",
            to: entity.assignee.email,
