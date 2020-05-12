09423542bb88217e6963ae70351056d799fb20c8
Bill Transue <transue@gmail.com>
2020-05-12 12:15:12 -0400

    Namespace ListSpec

--- spec/helpers/tasks_helper_spec.rb
+++ spec/helpers/tasks_helper_spec.rb
@@ -5,18 +5,19 @@
 # Fat Free CRM is freely distributable under the terms of MIT license.
 # See MIT-LICENSE file or http://www.opensource.org/licenses/mit-license.php
 #------------------------------------------------------------------------------
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
 module FatFreeCrm
-describe TasksHelper do
-  describe "responding with generated links" do
-    before do
-      @task = create(:task)
-    end
+  describe TasksHelper, type: :helper do
+    describe "responding with generated links" do
+      before do
+        @task = create(:task)
+        FatFreeCrm::TasksHelper.extend(::FatFreeCrm::Engine.routes.url_helpers)
+      end
 
-    it "should render link to uncomplete of a task" do
-      expect(link_to_task_uncomplete(@task, nil)).to include(t(:task_uncomplete))
+      it "should render link to uncomplete of a task" do
+        expect(FatFreeCrm::TasksHelper.extend(::FatFreeCrm::Engine.routes.url_helpers).link_to_task_uncomplete(@task, nil)).to include(t(:task_uncomplete))
+      end
     end
   end
-end
 end
