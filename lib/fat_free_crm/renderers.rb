9ba556341960fe5e7f3a38c0252dadbb6b972839
Bill Transue <transue@gmail.com>
2020-05-13 11:50:54 -0400

    Rename ExportCSV to ExportCsv

--- lib/fat_free_crm/renderers.rb
+++ lib/fat_free_crm/renderers.rb
@@ -7,7 +7,7 @@
 #------------------------------------------------------------------------------
 ActionController::Renderers.add :csv do |objects, options|
   filename = options[:filename] || controller_name || 'data'
-  str = FatFreeCrm::ExportCSV.from_array(objects)
+  str = FatFreeCrm::ExportCsv.from_array(objects)
   send_data str, type: :csv,
                  disposition: "attachment; filename=#{filename}.csv"
 end
