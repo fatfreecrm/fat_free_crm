c5c30a29fbbb0c1bc6a4cd6416e9a985431b6590
Daniel O'Connor <daniel.oconnor@gmail.com>
2020-05-28 17:38:46 +0930

    Add print.css to assets to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( print.css )
