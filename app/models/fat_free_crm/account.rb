99701be5d6534c39bcf4c787fcdebef60171e852
zasman <zanesandin@gmail.com>
2020-06-07 23:55:57 -0400

    removes uniqueness constraint for account name

--- app/models/fat_free_crm/account.rb
+++ app/models/fat_free_crm/account.rb
@@ -83,7 +83,7 @@ module FatFreeCrm
     ransack_can_autocomplete
 
     validates_presence_of :name, message: :missing_account_name
-    validates_uniqueness_of :name, scope: :deleted_at, if: -> { Setting.require_unique_account_names }
+    # validates_uniqueness_of :name, scope: :deleted_at, if: -> { Setting.require_unique_account_names }
     validates :rating, inclusion: { in: 0..5 }, allow_blank: true
     validates :category, inclusion: { in: proc { Setting.unroll(:account_category).map { |s| s.last.to_s } } }, allow_blank: true
     validate :users_for_shared_access
