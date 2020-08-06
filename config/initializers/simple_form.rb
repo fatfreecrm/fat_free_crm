8823bbe682c664742f72f79c5f37d4b176920346
Daniel O'Connor <daniel.oconnor@gmail.com>
2020-08-06 12:49:55 +0930

    Enable browser validations by default

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -132,7 +132,7 @@ SimpleForm.setup do |config|
   # in this configuration, which is recommended due to some quirks from different browsers.
   # To stop SimpleForm from generating the novalidate option, enabling the HTML5 validations,
   # change this configuration to true.
-  config.browser_validations = false
+  config.browser_validations = true
 
   # Custom mappings for input types. This should be a hash containing a regexp
   # to match as key, and the input type that will be used when the field name
