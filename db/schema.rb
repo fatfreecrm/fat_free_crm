a0473436c8374d0f9c86eb2bbdbf3d78ed91d496
Rubisel Prieto Dupeyron <rubiselprieto@gmail.com>
2020-12-21 07:54:23 -0500

    Remove extension in shema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_12_17_030615) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "account_contacts", id: :serial, force: :cascade do |t|
     t.integer "account_id"
     t.integer "contact_id"
