97b3e29b7ec38075bca5b0a2a9f8de2d47d9528e
Luilver Garces <luilver@gmail.com>
2021-02-26 09:56:02 -0500

    Fix some offenses

--- spec/features/acceptance_helper.rb
+++ spec/features/acceptance_helper.rb
@@ -8,4 +8,4 @@
 require 'spec_helper'
 
 # Put your acceptance spec helpers inside /spec/features/support
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
