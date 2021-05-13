12466d955bceb7a58d67b5313d873e3aa123ed04
John Bumgardner <john.bumgardner@icloud.com>
2021-05-13 19:48:41 -0400

    add username generator

--- spec/factories/user_factories.rb
+++ spec/factories/user_factories.rb
@@ -7,7 +7,7 @@
 #------------------------------------------------------------------------------
 FactoryBot.define do
   factory :user do
-    username            { FactoryBot.generate(:username) }
+    username            { FFaker::Internet.user_name }
     email               { FFaker::Internet.email }
     first_name          { FFaker::Name.first_name }
     last_name           { FFaker::Name.last_name }
