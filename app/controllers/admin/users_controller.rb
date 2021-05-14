ae8bc11a1d8d3fdf421ddd0d4850b808ce1129ba
John Bumgardner <john.bumgardner@icloud.com>
2021-05-14 10:02:30 -0400

    try using require

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -107,7 +107,7 @@ class Admin::UsersController < Admin::ApplicationController
     params[:user][:email].try(:strip!)
     params[:user][:password_confirmation] = nil if params[:user][:password_confirmation].blank?
 
-    params[:user].permit!(
+    params[:user].require(:admin).permit(
       :admin,
       :username,
       :email,
