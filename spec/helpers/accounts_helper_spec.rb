325e954d68e9af70f4d9181e2b6733c74434d9a8
John Bumgardner <john.bumgardner@icloud.com>
2021-05-16 13:54:53 -0400

    add tests

--- spec/helpers/accounts_helper_spec.rb
+++ spec/helpers/accounts_helper_spec.rb
@@ -13,4 +13,40 @@ describe AccountsHelper do
     included_modules = (class << helper; self; end).send :included_modules
     expect(included_modules).to include(AccountsHelper)
   end
+  describe '#display_pipeline' do
+    context 'when the pipeline is zero' do
+      it 'returns "N/A"' do
+        expect(display_pipeline(0)).to eq('N/A')
+      end
+    end
+    context 'when the pipeline is has 300' do
+      it 'returns "$300"' do
+        expect(display_pipeline(300)).to eq('$300')
+      end
+    end
+  end
+  describe '#display_won' do
+    context 'when the won is zero' do
+      it 'returns "N/A"' do
+        expect(display_won(0)).to eq('N/A')
+      end
+    end
+    context 'when the won is has 300' do
+      it 'returns "$300"' do
+        expect(display_won(300)).to eq('$300')
+      end
+    end
+  end
+  describe '#display_lost' do
+    context 'when the lost is zero' do
+      it 'returns "N/A"' do
+        expect(display_lost(0)).to eq('N/A')
+      end
+    end
+    context 'when the lost is has 300' do
+      it 'returns "$300"' do
+        expect(display_lost(300)).to eq('$300')
+      end
+    end
+  end
 end
