81fe30b7b4b5f311de9a82236c15246f664ecab8
Daniel O'Connor <daniel.oconnor@gmail.com>
2021-10-04 15:43:09 +1030

    Fix rubocop warnings

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -130,7 +130,7 @@ describe UsersController do
         expect(response).to render_template("users/update")
       end
     end
-    
+
     describe "with a email with whitespace" do
       it "should update the user information and render [update] template" do
         put :update, params: { id: @user.id, user: { first_name: "Billy", last_name: "Bones", alt_email: " john@email.com " } }, xhr: true
@@ -140,7 +140,7 @@ describe UsersController do
         expect(assigns[:user]).to eq(@user)
         expect(response).to render_template("users/update")
         expect(@user.alt_email).to eq("john@email.com")
-      end      
+      end
     end
 
     describe "with invalid params" do
