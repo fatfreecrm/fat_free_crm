7afd001a25070de9adf59a56a4f1f4dce1078815
John Bumgardner <jbumgard@cisco.com>
2023-05-27 16:42:08 -0400

    try using with rack attached

--- spec/factories/shared_factories.rb
+++ spec/factories/shared_factories.rb
@@ -63,7 +63,7 @@ FactoryBot.define do
   factory :avatar do
     user
     entity              { raise "Please specify :entity for the avatar" }
-    image               { file_fixture('rails.png').open }
+    image               { Rack::Test::UploadedFile.new("spec/fixtures/files/rails.png", "image/png") }
     updated_at          { FactoryBot.generate(:time) }
     created_at          { FactoryBot.generate(:time) }
   end
---
+++ spec/support/active_storage.rb
@@ -0,0 +1,8 @@
+# spec/support/active_storage.rb
+RSpec.configure do |config|
+    config.after(:each) do
+      # Clear uploaded files after each test
+      ActiveStorage::Blob.all.each(&:purge)
+    end
+  end
+  
