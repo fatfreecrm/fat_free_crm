4018055b9c796f1832fd76a22d2a26683af57078
Stephen Kenworthy <steveyken@gmail.com>
2023-06-06 14:58:46 +0800

    Fixes error raised when running rake db:create db:migrate for first time.

--- lib/fat_free_crm/fields.rb
+++ lib/fat_free_crm/fields.rb
@@ -24,7 +24,7 @@ module FatFreeCRM
 
     module SingletonMethods
       def field_groups
-        if ActiveRecord::Base.connection.data_source_exists? 'field_groups'
+        if (ActiveRecord::Base.connection.data_source_exists?('field_groups') rescue false)
           FieldGroup.where(klass_name: name).order(:position)
         else
           []
