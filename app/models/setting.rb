7c13e937560481a35e65b06eaaf8d325e27f6c26
Daniel O'Connor <daniel.oconnor@gmail.com>
2023-12-04 23:30:13 +1030

    Adjust rubocop (#958)
    * Bump to ruby 2.7
    
    * Rubocop
    
    * Rubocop
    
    * Rubocop
    
    * Install
    
    * Install, a lot

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -58,11 +58,9 @@ class Setting < ActiveRecord::Base
       return cache[name] if cache.key?(name)
 
       # Check database
-      if database_and_table_exists?
-        if setting = find_by_name(name.to_s)
-          return cache[name] = setting.value unless setting.value.nil?
+      if database_and_table_exists? && setting = find_by_name(name.to_s) && !setting.value.nil?
+        return cache[name] = setting.value
         end
-      end
       # Check YAML settings
       return cache[name] = yaml_settings[name] if yaml_settings.key?(name)
     end
@@ -73,6 +71,7 @@ class Setting < ActiveRecord::Base
       raise ArgumentError, "name cannot be blank" if name.blank?
 
       return nil unless database_and_table_exists?
+
       setting = find_by_name(name.to_s) || new(name: name.to_s)
       setting.value = value
       setting.save
