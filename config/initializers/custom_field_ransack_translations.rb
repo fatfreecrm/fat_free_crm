ddc3efc4dce8c8abf3703e37b7d5cf2d3047ee39
Steve Kenworthy <steveyken@gmail.com>
2024-02-24 09:46:11 +0800

    Fix load paths - this works for both Application and Engine mode. (#1240)
    Co-authored-by: Daniel O'Connor <daniel.oconnor@gmail.com>

--- config/initializers/custom_field_ransack_translations.rb
+++ config/initializers/custom_field_ransack_translations.rb
@@ -6,7 +6,7 @@
 # See MIT-LICENSE file or http://www.opensource.org/licenses/mit-license.php
 #------------------------------------------------------------------------------
 # Load field names for custom fields, for Ransack search
-require './app/models/setting'
+require 'setting'
 if Setting.database_and_table_exists? && ActiveRecord::Base.connection.table_exists?(:custom_fields)
   Rails.application.config.after_initialize do
     I18n.backend.load_translations
