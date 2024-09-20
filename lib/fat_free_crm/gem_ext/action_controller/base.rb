ca4e08767961bf1dd4f360f119d43be62e36218e
Stanley <stanley321@gmail.com>
2024-09-20 15:50:47 +0800

    Remove Test ENV condition

--- lib/fat_free_crm/gem_ext/action_controller/base.rb
+++ lib/fat_free_crm/gem_ext/action_controller/base.rb
@@ -5,14 +5,12 @@
 # Fat Free CRM is freely distributable under the terms of MIT license.
 # See MIT-LICENSE file or http://www.opensource.org/licenses/mit-license.php
 #------------------------------------------------------------------------------
-unless Rails.env.test?
-  class ActionController::Base
-    # Remove helpers residing in subdirectories from the list of application
-    # helpers.  Basically we don't want helpers in app/helpers/admin/* to
-    # override the ones in app/helpers/*.
-    #----------------------------------------------------------------------------
-    def self.all_application_helpers
-      super.delete_if { |helper| helper.include?(File::SEPARATOR) }
-    end
+class ActionController::Base
+  # Remove helpers residing in subdirectories from the list of application
+  # helpers.  Basically we don't want helpers in app/helpers/admin/* to
+  # override the ones in app/helpers/*.
+  #----------------------------------------------------------------------------
+  def self.all_application_helpers
+    super.delete_if { |helper| helper.include?(File::SEPARATOR) }
   end
 end
