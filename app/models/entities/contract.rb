0226f1b11289a013f9b4aa6c6c0d5647698c551d
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-07-26 09:45:06 +0000

    Exportable

--- app/models/entities/contract.rb
+++ app/models/entities/contract.rb
@@ -2,4 +2,7 @@ class Contract < ActiveRecord::Base
   belongs_to :account
   has_many :contracted_products
   has_many :products, through: :contracted_products
+
+  exportable
+  sortable by: ["created_at DESC", "updated_at DESC"], default: "created_at DESC"
 end
