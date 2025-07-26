1f9d4969d0985c663de1acdab65dbb9e2ada079b
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-07-26 04:03:57 +0000

    Fix devcontainer

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -2,10 +2,10 @@
 
 sudo add-apt-repository -y ppa:mozillateam/ppa
 sudo apt-get update
-sudo apt install libpq-dev # Not strictly needed, but easier than explaining to users to constantly pass DB=sqlite
+sudo apt install -y libpq-dev # Not strictly needed, but easier than explaining to users to constantly pass DB=sqlite
 
 # TODO: https://gist.github.com/jfeilbach/78d0ef94190fb07dee9ebfc34094702f
-sudo apt-get install firefox 
+sudo apt-get install -y firefox 
 
 bundle
 
@@ -15,3 +15,5 @@ bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
 bundle exec rake ffcrm:demo:load
+
+bundle exec rails s -b 0.0.0.0
