33b3867569a06ac900cc931dfbc360d8d82ce6af
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-07-26 04:42:03 +0000

    Style/RedundantRegexpEscape: Redundant escape inside regexp literal

--- app/models/users/user.rb
+++ app/models/users/user.rb
@@ -73,7 +73,7 @@ class User < ActiveRecord::Base
   scope :by_name, -> { order('first_name, last_name, email') }
 
   scope :text_search, lambda { |query|
-    query = query.gsub(/[^\w\s\-\.'\p{L}]/u, '').strip
+    query = query.gsub(/[^\w\s\-.'\p{L}]/u, '').strip
     where('upper(username) LIKE upper(:s) OR upper(email) LIKE upper(:s) OR upper(first_name) LIKE upper(:s) OR upper(last_name) LIKE upper(:s)', s: "%#{query}%")
   }
 
