fdb7282e92e5cca1856cc51b4b5a93be7016668b
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-07-26 10:42:28 +0000

    Add more views

--- app/models/entities/product.rb
+++ app/models/entities/product.rb
@@ -5,6 +5,11 @@ class Product < ActiveRecord::Base
   belongs_to :user, optional: true # TODO: Is this really optional?
   belongs_to :assignee, class_name: "User", foreign_key: :assigned_to, optional: true
 
+  uses_user_permissions
+  acts_as_commentable
+  uses_comment_extensions
+  acts_as_taggable_on :tags
+  has_paper_trail versions: { class_name: 'Version' }, ignore: [:subscribed_users]
   exportable
   sortable by: ["name ASC", "created_at DESC", "updated_at DESC"], default: "created_at DESC"
   # :name, :sku, :description, :image_url, :url, :gtin, :brand
