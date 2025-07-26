d306d7e668891a3251f2f4faa084de19f8c5e9d7
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-07-26 04:46:44 +0000

    Rubocop: Spacing

--- spec/lib/fat_free_crm/i18n.rb
+++ spec/lib/fat_free_crm/i18n.rb
@@ -8,7 +8,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe 'I18n.t()' do
-
   class TestController < ActionController::Base
     include FatFreeCRM::I18n
   end
