7aa06b5c14be4896ac817a89d4824bead213e17a
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-07-27 11:18:43 +0930

    Bump version

--- lib/fat_free_crm/version.rb
+++ lib/fat_free_crm/version.rb
@@ -8,8 +8,8 @@
 module FatFreeCRM
   module VERSION # :nodoc:
     MAJOR = 0
-    MINOR = 22
-    TINY  = 1
+    MINOR = 23
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
