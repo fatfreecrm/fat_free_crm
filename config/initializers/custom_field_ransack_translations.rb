96344ec5716dcf7cf9d4e7abbc7974f83703601c
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-08-03 03:41:18 +0930

    Update custom_field_ransack_translations.rb

--- config/initializers/custom_field_ransack_translations.rb
+++ config/initializers/custom_field_ransack_translations.rb
@@ -6,9 +6,8 @@
 # See MIT-LICENSE file or http://www.opensource.org/licenses/mit-license.php
 #------------------------------------------------------------------------------
 # Load field names for custom fields, for Ransack search
-require 'setting'
-if Setting.database_and_table_exists? && ActiveRecord::Base.connection.table_exists?(:custom_fields)
-  Rails.application.config.after_initialize do
+
+Rails.application.config.after_initialize do
     I18n.backend.load_translations
 
     translations = { ransack: { attributes: {} } }
@@ -21,5 +20,5 @@ if Setting.database_and_table_exists? && ActiveRecord::Base.connection.table_exi
     end
 
     I18n.backend.store_translations(Setting.locale.to_sym, translations)
-  end
+
 end
