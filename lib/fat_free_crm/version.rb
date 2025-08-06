f09afc141890014e5777cafeca7212ac8343105a
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-08-06 09:37:24 +0000

    Fix version

--- lib/fat_free_crm/version.rb
+++ lib/fat_free_crm/version.rb
@@ -8,8 +8,8 @@
 module FatFreeCRM
   module VERSION # :nodoc:
     MAJOR = 0
-    MINOR = 24
-    TINY  = 1
+    MINOR = 25
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
