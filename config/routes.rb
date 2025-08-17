482e96646ae16a825873e7b6c1196a534712b4fa
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-08-17 13:46:08 +0000

    Slightly improve, but the form is not doing anything I want it to

--- config/routes.rb
+++ config/routes.rb
@@ -21,11 +21,11 @@ Rails.application.routes.draw do
                                     confirmations: 'confirmations' }
 
   devise_scope :user do
-    post 'sign_up/new_challenge', to: 'users/registrations#new_challenge', as: :new_user_registration_challenge
-    post 'sign_in/new_challenge', to: 'users/sessions#new_challenge', as: :new_user_session_challenge
+    post 'sign_up/new_challenge', to: 'registrations#new_challenge', as: :new_user_registration_challenge
+    post 'sign_in/new_challenge', to: 'sessions#new_challenge', as: :new_user_session_challenge
 
-    post 'reauthenticate/new_challenge', to: 'users/reauthentication#new_challenge', as: :new_user_reauthentication_challenge
-    post 'reauthenticate', to: 'users/reauthentication#reauthenticate', as: :user_reauthentication
+    post 'reauthenticate/new_challenge', to: 'reauthentication#new_challenge', as: :new_user_reauthentication_challenge
+    post 'reauthenticate', to: 'reauthentication#reauthenticate', as: :user_reauthentication
 
     resources :users, only: %i[index show] do
       resources :passkeys, only: %i[index create destroy] do
