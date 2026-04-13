3a2e24e6d5de4f2988d2ad200d661d4e4161b3ed
Daniel O'Connor <daniel.oconnor@gmail.com>
2026-04-13 04:48:39 +0000

    Rubocop

--- db/migrate/20260412040909_add_twitter_and_linkedin_to_users.rb
+++ db/migrate/20260412040909_add_twitter_and_linkedin_to_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddTwitterAndLinkedinToUsers < ActiveRecord::Migration[7.1]
   def change
     change_table :users do |t|
--- db/migrate/20260413040024_add_social_media_to_accounts.rb
+++ db/migrate/20260413040024_add_social_media_to_accounts.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddSocialMediaToAccounts < ActiveRecord::Migration[8.0]
   def change
     add_column :accounts, :blog, :string
